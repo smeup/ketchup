b7196e26e5276e9dbf982b12f7dcff0c864ce976
casetta-smeup <francesco.casetta@smeup.com>
2019-04-04 11:45:34 +0200

    packages version update / added .prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 2,
+    "overrides": [
+        {
+            "files": "*.css",
+            "options": {
+                "tabWidth": 4
+            }
+        }
+    ]
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+    proseWrap: 'never',
+    htmlWhitespaceSensitivity: 'strict',
+    endOfLine: 'lf',
+};
