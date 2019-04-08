97d9af926db15c65037d381d6bbfe58789890744
casetta-smeup <francesco.casetta@smeup.com>
2019-04-08 10:08:02 +0200

    tests

--- packages/ketchup/src/components/ketchup-button/ketchup-button.e2e.ts
+++ packages/ketchup/src/components/ketchup-button/ketchup-button.e2e.ts
@@ -1,73 +1,189 @@
-import { newE2EPage } from '@stencil/core/testing'
+import { newE2EPage } from '@stencil/core/testing';
+
+const alignRight = 'align-right';
+const alignLeft = 'align-left';
+const fillspace = 'fillspace';
+const flat = 'flat-btn';
+const rounded = 'rounded';
+const transparent = 'transparent';
 
 describe('ketchup-button', () => {
     it('renders', async () => {
-        const page = await newE2EPage()
+        const page = await newE2EPage();
 
-        await page.setContent('<ketchup-button></ketchup-button>')
-        const element = await page.find('ketchup-button')
-        expect(element).toHaveClass('hydrated')
-    })
+        await page.setContent('<ketchup-button></ketchup-button>');
+        const element = await page.find('ketchup-button');
+        expect(element).toHaveClass('hydrated');
+    });
 
     it('testing default', async () => {
-        const page = await newE2EPage()
+        const page = await newE2EPage();
 
-        await page.setContent('<ketchup-button></ketchup-button>')
+        await page.setContent('<ketchup-button></ketchup-button>');
 
-        const buttonText = await page.find('ketchup-button button .button-text')
-        expect(buttonText).toBeNull()
+        const buttonText = await page.find(
+            'ketchup-button >>> button .button-text'
+        );
+        expect(buttonText).toBeNull();
 
-        const buttonIcon = await page.find('ketchup-button button .button-icon')
-        expect(buttonIcon).toBeNull()
+        const buttonIcon = await page.find(
+            'ketchup-button >>> button .button-icon'
+        );
+        expect(buttonIcon).toBeNull();
 
         // testing default classes
-        const button = await page.find('ketchup-button button')
+        const button = await page.find('ketchup-button >>> button');
 
-        expect(button.classList.contains('flat-btn')).toBeFalsy()
-        expect(button.classList.contains('fillspace')).toBeFalsy()
-        expect(button.classList.contains('rounded')).toBeFalsy()
-        expect(button.classList.contains('transparent')).toBeFalsy()
-        expect(button.classList.contains('align-right')).toBeFalsy()
-        expect(button.classList.contains('align-left')).toBeFalsy()
+        expect(button).toBeTruthy();
+
+        expect(button).not.toHaveClasses([
+            flat,
+            fillspace,
+            rounded,
+            transparent,
+            alignRight,
+            alignLeft,
+        ]);
 
         // no title
-        expect(button.getAttribute('title')).toEqual('')
-    })
+        expect(button.getAttribute('title')).toEqual('');
+    });
 
     it('should have text and icon', async () => {
-        const page = await newE2EPage()
+        const page = await newE2EPage();
 
-        const iconClass = 'mdi mdi-account'
+        const iconClass = 'mdi mdi-account';
 
         await page.setContent(
             `<ketchup-button label="Pippo" icon-class="${iconClass}"></ketchup-button>`
-        )
+        );
 
-        const buttonText = await page.find('ketchup-button button .button-text')
-        expect(buttonText.textContent).toEqual('Pippo')
+        const buttonText = await page.find(
+            'ketchup-button >>> button .button-text'
+        );
+        expect(buttonText).toEqualText('Pippo');
 
-        const buttonIcon = await page.find('ketchup-button button .button-icon')
-        expect(buttonIcon.className).toEqual('button-icon ' + iconClass)
-    })
+        const buttonIcon = await page.find(
+            'ketchup-button >>> button .button-icon'
+        );
+
+        expect(buttonIcon.className).toEqual('button-icon ' + iconClass);
+    });
 
     it('should be flat', async () => {
-        const page = await newE2EPage()
+        const page = await newE2EPage();
 
-        await page.setContent('<ketchup-button flat></ketchup-button>')
+        await page.setContent('<ketchup-button flat></ketchup-button>');
 
-        const button = await page.find('ketchup-button button')
-        expect(button.classList.contains('flat-btn')).toBeTruthy()
-    })
+        const button = await page.find('ketchup-button >>> button');
+        expect(button).toHaveClass(flat);
+    });
 
     it('should have a custom class', async () => {
-        const page = await newE2EPage()
-        const buttonClass = 'my-custom-class'
+        const page = await newE2EPage();
+        const buttonClass = 'my-custom-class';
 
         await page.setContent(
             `<ketchup-button button-class="${buttonClass}"></ketchup-button>`
-        )
+        );
 
-        const button = await page.find('ketchup-button button')
-        expect(button.classList.contains(buttonClass)).toBeTruthy()
-    })
-})
+        const button = await page.find('ketchup-button >>> button');
+        expect(button).toHaveClass(buttonClass);
+    });
+
+    it('has fillspace class', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent(`<ketchup-button fillspace></ketchup-button>`);
+
+        const button = await page.find('ketchup-button >>> button');
+        expect(button).toHaveClass(fillspace);
+    });
+
+    it('should hide text', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent(
+            `<ketchup-button showtext="false" label="Pippo"></ketchup-button>`
+        );
+
+        const buttonText = await page.find(
+            'ketchup-button >>> button .button-text'
+        );
+        expect(buttonText).toBeNull();
+    });
+
+    it('should hide icon', async () => {
+        const page = await newE2EPage();
+
+        const iconClass = 'mdi mdi-account';
+
+        await page.setContent(
+            `<ketchup-button showicon="false" icon-class="${iconClass}"></ketchup-button>`
+        );
+
+        const buttonIcon = await page.find(
+            'ketchup-button >>> button .button-icon'
+        );
+        expect(buttonIcon).toBeNull();
+    });
+
+    it('should be rounded', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent(`<ketchup-button rounded></ketchup-button>`);
+
+        const button = await page.find('ketchup-button >>> button');
+        expect(button).toHaveClass(rounded);
+    });
+
+    it('textmode hint', async () => {
+        const page = await newE2EPage();
+
+        const label = 'Pippo';
+
+        await page.setContent(
+            `<ketchup-button label="${label}" textmode="Hint"></ketchup-button>`
+        );
+
+        const button = await page.find('ketchup-button >>> button');
+        expect(button).not.toBeNull();
+        expect(button.getAttribute('title')).toEqual(label);
+
+        const buttonText = await page.find(
+            'ketchup-button >>> button .button-text'
+        );
+        expect(buttonText).toBeNull();
+    });
+
+    it('should be transparent', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent(`<ketchup-button transparent></ketchup-button>`);
+
+        const button = await page.find('ketchup-button >>> button');
+        expect(button).toHaveClass(transparent);
+    });
+
+    it('should be right aligned', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent(
+            `<ketchup-button align="right"></ketchup-button>`
+        );
+
+        const button = await page.find('ketchup-button >>> button');
+        expect(button).toHaveClass(alignRight);
+    });
+
+    it('should be left aligned', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent(`<ketchup-button align="left"></ketchup-button>`);
+
+        const button = await page.find('ketchup-button >>> button');
+        expect(button).toHaveClass(alignLeft);
+    });
+
+    // TODO testare click
+});
--- packages/ketchup/src/components/ketchup-button/ketchup-button.tsx
+++ packages/ketchup/src/components/ketchup-button/ketchup-button.tsx
@@ -1,105 +1,99 @@
-import {
-    Component,
-    Element,
-    Event,
-    EventEmitter,
-    Prop
-} from '@stencil/core'
+import { Component, Element, Event, EventEmitter, Prop } from '@stencil/core';
 
 @Component({
     tag: 'ketchup-button',
     styleUrl: 'ketchup-button.scss',
-    shadow: true
+    shadow: true,
 })
 export class KetchupButton {
-    @Element() ketchupButtonEl: HTMLElement
+    @Element() ketchupButtonEl: HTMLElement;
 
-    @Prop() flat = false
-    @Prop() label: string
-    @Prop() buttonClass: string
-    @Prop() iconClass: string
-    @Prop() fillspace = false
-    @Prop() showtext = true
-    @Prop() showicon = true
-    @Prop() rounded = false
-    @Prop() textmode: string
-    @Prop() transparent = false
-    @Prop() align: string
+    @Prop() flat = false;
+    @Prop() label: string;
+    @Prop() buttonClass: string;
+    @Prop() iconClass: string;
+    @Prop() fillspace = false;
+    @Prop() showtext = true;
+    @Prop() showicon = true;
+    @Prop() rounded = false;
+    @Prop() textmode: string;
+    @Prop() transparent = false;
+    @Prop() align: string;
     @Prop() iconUrl =
-        'https://cdn.materialdesignicons.com/3.2.89/css/materialdesignicons.min.css'
+        'https://cdn.materialdesignicons.com/3.2.89/css/materialdesignicons.min.css';
 
     @Event({
         eventName: 'ketchupButtonClicked',
         composed: true,
         cancelable: true,
-        bubbles: true
+        bubbles: true,
     })
     ketchupButtonClicked: EventEmitter<{
         id: string;
     }>;
 
     onBtnClickedHandler() {
-        this.ketchupButtonClicked.emit({id: this.ketchupButtonEl.dataset.id})
+        this.ketchupButtonClicked.emit({ id: this.ketchupButtonEl.dataset.id });
     }
 
     _isHint() {
-        return 'Hint' === this.textmode
+        return 'Hint' === this.textmode;
     }
 
     render() {
-        let btnLabel = null
+        let btnLabel = null;
         if (
             (!this._isHint() || (this._isHint() && this.flat)) &&
             this.showtext &&
             this.label
         ) {
-            btnLabel = <span class="button-text">{this.label}</span>
+            btnLabel = <span class="button-text">{this.label}</span>;
         }
 
-        let icon = null
+        let icon = null;
         if (this.showicon && this.iconClass) {
-            icon = <span class={'button-icon ' + this.iconClass}/>
+            icon = <span class={'button-icon ' + this.iconClass} />;
         }
 
-        let btnClass = ''
+        let btnClass = '';
         if (this.flat) {
-            btnClass = 'flat-btn'
+            btnClass = 'flat-btn';
         } else {
             if (this.buttonClass) {
-                btnClass += this.buttonClass
+                btnClass += this.buttonClass;
             }
 
             if (this.rounded) {
-                btnClass += ' rounded'
+                btnClass += ' rounded';
             }
 
             if (this.transparent) {
-                btnClass += ' transparent'
+                btnClass += ' transparent';
             }
         }
 
         if (this.fillspace) {
-            btnClass += ' fillspace'
+            btnClass += ' fillspace';
         }
 
         if (this.align) {
             if ('right' === this.align) {
-                btnClass += ' align-right'
+                btnClass += ' align-right';
             } else if ('left' === this.align) {
-                btnClass += ' align-left'
+                btnClass += ' align-left';
             }
         }
 
-        btnClass = btnClass.trim()
+        btnClass = btnClass.trim();
 
-        let title = ''
-        if ('Hint' === this.textmode) {
-            title = this.label
+        let title = '';
+        if (this._isHint()) {
+            title = this.label;
         }
 
         // TODO: check if the div element can be removed by passing JSX an array of elements
         return [
-            <link href={this.iconUrl} rel="stylesheet" type="text/css"/>,
+            <link href={this.iconUrl} rel="stylesheet" type="text/css" />,
             <button
                 class={btnClass}
                 title={title}
@@ -107,7 +101,7 @@ export class KetchupButton {
             >
                 {icon}
                 {btnLabel}
-            </button>
-        ]
+            </button>,
+        ];
     }
 }
