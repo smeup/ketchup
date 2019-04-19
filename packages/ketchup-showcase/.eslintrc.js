9168065619acddc6154f70426983d39a4e116eb1
Chiara Zambelli <chiara.zambelli@smeup.com>
2019-04-19 10:36:40 +0200

    substituted ketchup-showcase with ketchup-showcase-ts

--- packages/ketchup-showcase/.eslintrc.js
+++ packages/ketchup-showcase/.eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+
+  extends: ["plugin:vue/essential", "@vue/prettier"],
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
+
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: "typescript-eslint-parser"
+  },
+
+  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"]
+};
--- packages/ketchup-showcase/tests/e2e/plugins/index.js
+++ packages/ketchup-showcase/tests/e2e/plugins/index.js
@@ -5,7 +5,7 @@
 // as explained in the cypress docs
 // https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
 
-/* eslint-disable import/no-extraneous-dependencies, global-require, arrow-body-style */
+// /* eslint-disable import/no-extraneous-dependencies, global-require */
 // const webpack = require('@cypress/webpack-preprocessor')
 
 module.exports = (on, config) => {
@@ -13,12 +13,4 @@ module.exports = (on, config) => {
   //  webpackOptions: require('@vue/cli-service/webpack.config'),
   //  watchOptions: {}
   // }))
-
-  return Object.assign({}, config, {
-    fixturesFolder: 'tests/e2e/fixtures',
-    integrationFolder: 'tests/e2e/specs',
-    screenshotsFolder: 'tests/e2e/screenshots',
-    videosFolder: 'tests/e2e/videos',
-    supportFile: 'tests/e2e/support/index.js'
-  })
-}
+};
