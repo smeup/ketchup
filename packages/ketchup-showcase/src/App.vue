e606dc9bcfab638fde3903784a4e039b7ffd2e13
Chiara Zambelli <chiara.zambelli@smeup.com>
2019-04-19 12:48:45 +0200

    added root scripts, make ksc unit tests and e2e working

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
 
-    <img alt="Vue logo" src="./assets/logo.png">
-    <ketchup-fld id="fld1" first="Runaways" last="'Call me any time"></ketchup-fld>
-    <ketchup-button label="Sono bottone"></ketchup-button>
-    <ketchup-radio id="krg1"></ketchup-radio>
-    <ketchup-text-input id="int1" is-clearable initial-value="ciao"></ketchup-text-input>
-    <ketchup-combo initial-value="sono di prova" is-clearable></ketchup-combo>
-    
     <SmeupLabel
       content="Test"
       color="#CCCCCC"
@@ -36,6 +29,12 @@
       @rowselected="onRowSelected"
     />
     <MAT :content="matrixData">CIAO</MAT>
+
+    <ketchup-fld id="fld1" first="Runaways" last="'Call me any time"></ketchup-fld>
+    <ketchup-button label="Sono bottone"></ketchup-button>
+    <ketchup-radio id="krg1"></ketchup-radio>
+    <ketchup-text-input id="int1" is-clearable initial-value="ciao"></ketchup-text-input>
+    <ketchup-combo initial-value="sono di prova" is-clearable></ketchup-combo>
 
   </div>
 </template>
