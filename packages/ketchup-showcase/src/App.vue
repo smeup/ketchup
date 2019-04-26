95da9391337b18fc9c0a33529535143553ac258e
Chiara Zambelli <chiara.zambelli@smeup.com>
2019-04-26 12:16:31 +0200

    fixed e2e

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -1,51 +1,53 @@
 <template>
     <!-- data-app attribute is MANDATORY to allow Vuetify to correctly install and work -->
     <div id="app" data-app>
-        <SmeupLabel
-            content="Test"
-            color="#CCCCCC"
-            align="center"
-            @click="onLabelClick"
-        />
+        <div id="content">
+            <SmeupLabel
+                content="Test"
+                color="#CCCCCC"
+                align="center"
+                @click="onLabelClick"
+            />
 
-        <SmeupFld
-            id="fld1"
-            type="Itx"
-            initialValue="asd"
-            label="InputText"
-            showSubmit
-            mobile
-            clearValue
-            @change="onFldChange"
-            @click="onFldClick"
-        />
+            <SmeupFld
+                id="fld1"
+                type="Itx"
+                initialValue="asd"
+                label="InputText"
+                showSubmit
+                mobile
+                clearValue
+                @change="onFldChange"
+                @click="onFldClick"
+            />
 
-        <SmeupMatrix
-            :filterable="true"
-            :sortable="true"
-            :grouping="false"
-            :data="data"
-            :selRecord="1"
-            @rowselected="onRowSelected"
-        />
-        <MAT :content="matrixData">CIAO</MAT>
+            <SmeupMatrix
+                :filterable="true"
+                :sortable="true"
+                :grouping="false"
+                :data="data"
+                :selRecord="1"
+                @rowselected="onRowSelected"
+            />
+            <MAT :content="matrixData">CIAO</MAT>
 
-        <ketchup-fld
-            id="fld1"
-            first="Runaways"
-            last="'Call me any time"
-        ></ketchup-fld>
-        <ketchup-button label="Sono bottone"></ketchup-button>
-        <ketchup-radio id="krg1"></ketchup-radio>
-        <ketchup-text-input
-            id="int1"
-            is-clearable
-            initial-value="ciao"
-        ></ketchup-text-input>
-        <ketchup-combo
-            initial-value="sono di prova"
-            is-clearable
-        ></ketchup-combo>
+            <ketchup-fld
+                id="fld1"
+                first="Runaways"
+                last="'Call me any time"
+            ></ketchup-fld>
+            <ketchup-button label="Sono bottone"></ketchup-button>
+            <ketchup-radio id="krg1"></ketchup-radio>
+            <ketchup-text-input
+                id="int1"
+                is-clearable
+                initial-value="ciao"
+            ></ketchup-text-input>
+            <ketchup-combo
+                initial-value="sono di prova"
+                is-clearable
+            ></ketchup-combo>
+        </div>
     </div>
 </template>
 
--- packages/ketchup-showcase/tests/e2e/specs/app1T.js
+++ packages/ketchup-showcase/tests/e2e/specs/app1T.js
@@ -1,14 +1,14 @@
 describe('Showcase e2e - sample test 1', () => {
     it('Visits the app root url 1', () => {
         cy.visit('/');
-        cy.get('#app')
+        cy.get('#content')
             .children()
             .first()
             .should('contain', 'Test');
     });
     it('Visits the app root url 1b', () => {
         cy.visit('/');
-        cy.get('#app')
+        cy.get('#content')
             .children()
             .first()
             .should('contain', 'Test');
