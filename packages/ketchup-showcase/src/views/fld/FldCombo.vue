9227c7fedb075e80add188eb8d5b5ba4143b4d42
Francesco Bonacini <f.bonacini@dreamonkey.com>
2019-06-05 18:46:23 +0200

    Inserted first data inside the Fld Showcase.

--- packages/ketchup-showcase/src/views/fld/FldCombo.vue
+++ packages/ketchup-showcase/src/views/fld/FldCombo.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    FLD Text input
+    <kup-fld
+      :config.prop="fldConfig"
+      :data.prop="fldData"/>
   </div>
 </template>
 
 <script>
   export default {
-    name: "FldCombo"
+    name: "FldCombo",
+    data() {
+      return {
+        fldConfig: {},
+        fldData: []
+      };
+    },
+    mounted() {
+      import('@/mock/fldData.ts')
+        .then(data => {
+          console.log(data);
+          const { fldData, fldConfig } = data;
+          console.log(fldData, fldConfig);
+          this.fldData = fldData;
+          this.fldConfig = fldConfig;
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
   }
 </script>
 
