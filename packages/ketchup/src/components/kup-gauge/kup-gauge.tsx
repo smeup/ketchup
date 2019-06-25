ff87a19b6fe0073392255e8d368654d26a603b1d
Francesco Bonacini <f.bonacini@dreamonkey.com>
2019-06-25 18:47:45 +0200

    Added reference, style and function to paint the needle.

--- packages/ketchup/src/components/kup-gauge/kup-gauge.tsx
+++ packages/ketchup/src/components/kup-gauge/kup-gauge.tsx
@@ -63,48 +63,71 @@ export class KupGauge {
     return deg * Math.PI / 180;
   };
 
+  paintNeedle(needleLength: number, needleBaseRadius: number, centerX: number, centerY: number) {
+    let leftX, leftY, rightX, rightY, thetaRad, topX, topY;
+    thetaRad = 0; //percToRad(perc / 2);
+    topX = centerX - needleLength * Math.cos(thetaRad);
+    topY = centerY - needleLength * Math.sin(thetaRad);
+    leftX = centerX - needleBaseRadius * Math.cos(thetaRad - Math.PI / 2);
+    leftY = centerY - needleBaseRadius * Math.sin(thetaRad - Math.PI / 2);
+    rightX = centerX - needleBaseRadius * Math.cos(thetaRad + Math.PI / 2);
+    rightY = centerY - needleBaseRadius * Math.sin(thetaRad + Math.PI / 2);
+    return "M " + leftX + " " + leftY + " L " + topX + " " + topY + " L " + rightX + " " + rightY;
+  }
 
 
   render() {
+    /*
     let perc = 45 / 100;
     let arcStartRad = this.percToRad(.75);
     let arcEndRad = arcStartRad + this.percToRad(perc / 2);
     let next_start = perc / 2;
 
     console.log(next_start, arcEndRad, arcStartRad);
+    */
+    const halvedSize = this.size / 2;
+    const needleCircleRadius = this.size / 16;
+    const needleLenght = halvedSize - 15;
 
     const a1 = this.arc1({
-      innerRadius: this.size / 2 - 30,
-      outerRadius: this.size / 2,
+      innerRadius: halvedSize - 30,
+      outerRadius: halvedSize,
       startAngle: 0,
       endAngle: Math.PI / 3
     });
     const a2 = this.arc2({
-      innerRadius: this.size / 2 - 30,
-      outerRadius: this.size / 2,
+      innerRadius: halvedSize - 30,
+      outerRadius: halvedSize,
       startAngle: Math.PI / 3,
       endAngle: Math.PI / 3 * 2
     });
     const a3 = this.arc3({
-      innerRadius: this.size / 2 - 30,
-      outerRadius: this.size / 2,
+      innerRadius: halvedSize - 30,
+      outerRadius: halvedSize,
       startAngle: Math.PI / 3 * 2,
       endAngle: Math.PI
     });
 
-    console.log(a1)
+    console.log(a1, halvedSize)
 
     return [
       <div
         // ref={(el) => (this.gaugeContainer = el as HTMLDivElement)}
       >
-        <svg
-          viewBox={`0 0 ${this.size} ${this.size / 2}`}>
-          <g transform={`rotate(-90) translate(-${this.size / 2}, ${this.size / 2})`}>
-            <path d={a1} style={{fill: this.colors[0] }}/>
-            <path d={a2} style={{fill: this.colors[1] }}/>
-            <path d={a3} style={{fill: this.colors[2] }}/>
+        <svg viewBox={`0 0 ${this.size} ${halvedSize}`}>
+          <g transform={`rotate(-90) translate(-${halvedSize}, ${halvedSize})`}>
+            <path d={a1} style={{ fill: this.colors[0] }}/>
+            <path d={a2} style={{ fill: this.colors[1] }}/>
+            <path d={a3} style={{ fill: this.colors[2] }}/>
           </g>
+          <circle
+            cx={halvedSize}
+            cy={halvedSize - needleCircleRadius}
+            r={needleCircleRadius}/>
+          <path
+            class="needle"
+            d={this.paintNeedle(needleLenght, needleCircleRadius, halvedSize, halvedSize - needleCircleRadius)}
+          />
         </svg>
       </div>,
       <p>{this.value}</p>
---
+++ packages/ketchup/src/components/kup-gauge/readme.md
@@ -0,0 +1,51 @@
+# kup-gauge
+
+### Useful references for this component
+
+https://codepen.io/enxaneta/pen/EVYRJJ
+
+##### Svg syntax
+https://css-tricks.com/transforms-on-svg-elements/
+https://css-tricks.com/svg-path-syntax-illustrated-guide/
+https://developer.mozilla.org/it/docs/Web/SVG/Element
+https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/transform
+
+##### Gauge examples
+https://plnkr.co/edit/LJlKKX1xnVwl2LoFBFoO?p=preview
+https://stackoverflow.com/questions/52373021/d3-js-add-label-along-the-arc
+
+##### D3 library reference
+https://github.com/d3?utf8=%E2%9C%93&q=&type=&language=
+https://github.com/d3/d3-shape#arcs
+https://github.com/d3/d3/blob/master/API.md
+
+##### React Style syntax
+https://stackoverflow.com/questions/26882177/react-js-inline-style-best-practices
+
+##### Less useful things
+http://www.pindari.com/svg-arc.html
+http://bl.ocks.org/msqr/3202712
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property          | Attribute          | Description | Type       | Default                      |
+| ----------------- | ------------------ | ----------- | ---------- | ---------------------------- |
+| `colors`          | --                 |             | `string[]` | `['red', 'yellow', 'green']` |
+| `firstThreshold`  | `first-threshold`  |             | `number`   | `0`                          |
+| `maxValue`        | `max-value`        |             | `number`   | `0`                          |
+| `minValue`        | `min-value`        |             | `number`   | `0`                          |
+| `secondThreshold` | `second-threshold` |             | `number`   | `0`                          |
+| `showLabels`      | `show-labels`      |             | `boolean`  | `false`                      |
+| `showValues`      | `show-values`      |             | `boolean`  | `true`                       |
+| `size`            | `size`             |             | `number`   | `200`                        |
+| `value`           | `value`            |             | `number`   | `0`                          |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
