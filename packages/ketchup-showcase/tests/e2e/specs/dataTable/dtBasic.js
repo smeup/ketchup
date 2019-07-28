d511bdb79748afd7074f04c1e055a263572a7e6d
Chiara Zambelli <chiara.zambelli@smeup.com>
2019-07-28 22:51:55 +0200

    introduced cypress-shadow-dom

--- packages/ketchup-showcase/tests/e2e/specs/dataTable/dtBasic.js
+++ packages/ketchup-showcase/tests/e2e/specs/dataTable/dtBasic.js
@@ -4,9 +4,37 @@ context('Data table without configuration', () => {
   //
   // });
 
-  it.skip('test tables', () => {
+  it('test first table', () => {
     cy.visit('http://localhost:4000/#/dataTable/basic');
 
-    cy.get('kup-data-table table').should('have.length', 4);
+    cy.shadowGet('kup-data-table')
+      .shadowFind('table')
+      .shadowFind('tr')
+      .its('length')
+      .should('eq', 4);
+
+    cy.shadowGet('kup-data-table')
+      .shadowFind('kup-paginator')
+      .shadowFind('.nextPageGroup')
+      .shadowEq(0)
+      .shadowContains('Numero risultati: 3');
+
+    cy.shadowGet('kup-data-table')
+      .shadowFind('.density-medium')
+      .its('length')
+      .should('eq', 1);
+
+    cy.shadowGet('kup-data-table')
+      .shadowFind('kup-button')
+      .shadowFind('button')
+      .shadowTrigger('click');
+
+    // cy.shadowFind don't seem to be repeated until a timeout like cy.get...  -> actually used a manual wait!
+    cy.wait(500);
+
+    cy.shadowGet('kup-data-table')
+      .shadowFind('.density-small')
+      .its('length')
+      .should('eq', 1);
   });
 });
