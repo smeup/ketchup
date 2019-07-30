2188619571ec0322417d334ff07f6f6980d443ac
Francesco Bonacini <f.bonacini@dreamonkey.com>
2019-07-30 14:02:09 +0200

    Fixed test for the badge with the new kup-badge version.

--- packages/ketchup/tests/e2e/box/box-badge.e2e.ts
+++ packages/ketchup/tests/e2e/box/box-badge.e2e.ts
@@ -27,47 +27,72 @@ describe('box and badges', () => {
         }
     });
 
-    it('with badges', async () => {
-        const page = await newE2EPage();
+    describe('with badges', async () => {
+        let page, element, boxes;
 
-        await page.setContent('<kup-box></kup-box>');
+        beforeEach(async () => {
+            page = await newE2EPage();
 
-        const element = await page.find('kup-box');
+            await page.setContent('<kup-box></kup-box>');
 
-        element.setProperty('data', badgeData);
+            element = await page.find('kup-box');
 
-        await page.waitForChanges();
+            element.setProperty('data', badgeData);
 
-        const boxes = await page.findAll(boxSelector);
+            await page.waitForChanges();
 
-        expect(boxes).toHaveLength(3);
+            boxes = await page.findAll(boxSelector);
+        });
 
-        for (let i = 0; i < boxes.length; i++) {
-            const box = boxes[i];
+        it('match badge content', async () => {
+            expect(boxes).toHaveLength(3);
 
-            const badges = await box.findAll('kup-badge >>> #badge');
+            for (let i = 0; i < boxes.length; i++) {
+                const box = boxes[i];
 
-            expect(badges).toHaveLength(1);
+                const badges = await box.findAll('kup-badge >>> #badge');
+                expect(badges).toHaveLength(1);
 
-            const badge = badges[0];
+                const badge = badges[0];
 
-            switch (i) {
-                case 0:
-                    expect(badge).toEqualText('1');
-                    expect(badge).toHaveClass('topRight');
-                    break;
+                switch (i) {
+                    case 0:
+                        expect(badge).toEqualText('1');
+                        break;
 
-                case 1:
-                    expect(badge).toHaveClass('mdi');
-                    expect(badge).toHaveClass('mdi-account');
-                    expect(badge).toHaveClass('topLeft');
-                    break;
+                    case 1:
+                        expect(badge).toHaveClass('mdi');
+                        expect(badge).toHaveClass('mdi-account');
+                        break;
 
-                default:
-                    expect(badge).toEqualText('long teeeeeeeeeeeeext');
-                    expect(badge).toHaveClass('topRight');
-                    break;
+                    default:
+                        expect(badge).toEqualText('long teeeeeeeeeeeeext');
+                        break;
+                }
             }
-        }
+        });
+
+        it('match badge class', async () => {
+            const kupBadges = await page.findAll('kup-box >>> kup-badge');
+
+            expect(kupBadges).toHaveLength(3);
+
+            for (let i = 0; i < kupBadges.length; i++) {
+                switch (i) {
+                    case 0:
+                        expect(kupBadges[i]).toHaveClass('top-right');
+                        break;
+
+                    case 1:
+                        console.log("secondo", kupBadges[i].className);
+                        expect(kupBadges[i]).toHaveClass('top-left');
+                        break;
+
+                    default:
+                        expect(kupBadges[i]).toHaveClass('top-right');
+                        break;
+                }
+            }
+        });
     });
 });
