3991dc23de3b02ac202b608599186558b755c82a
foscili-smeup <lucafoscili@icloud.com>
2019-10-16 15:09:18 +0200

    Restored tooltip at previous version

--- packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
+++ packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
@@ -1873,23 +1873,22 @@ export class KupDataTable {
         }
         if (this.hasTooltip(cell)) {
             content = (
-                <div>
-                    <span>{content}</span>
-                    <kup-tooltip
-                        onKupTooltipLoadData={(ev) =>
-                            this.kupLoadRequest.emit({
-                                cell: cell,
-                                tooltip: ev.srcElement,
-                            })
-                        }
-                        onKupTooltipLoadDetail={(ev) =>
-                            this.kupDetailRequest.emit({
-                                cell: cell,
-                                tooltip: ev.srcElement,
-                            })
-                        }
-                    ></kup-tooltip>
-                </div>
+                <kup-tooltip
+                    onKupTooltipLoadData={(ev) =>
+                        this.kupLoadRequest.emit({
+                            cell: cell,
+                            tooltip: ev.srcElement,
+                        })
+                    }
+                    onKupTooltipLoadDetail={(ev) =>
+                        this.kupDetailRequest.emit({
+                            cell: cell,
+                            tooltip: ev.srcElement,
+                        })
+                    }
+                >
+                    {content}
+                </kup-tooltip>
             );
         }
 
