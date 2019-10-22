4bbb493e2061f73baaab223f1cfe83600de64cef
Francesco Bonacini <f.bonacini@dreamonkey.com>
2019-10-22 15:07:43 +0200

    [kup-data-table] added test to allow testing of common filter paths.

--- packages/ketchup/tests/unit/data-table/data-table-filter.spec.ts
+++ packages/ketchup/tests/unit/data-table/data-table-filter.spec.ts
@@ -1,5 +1,5 @@
-import { filterRows } from '../../../src/components/kup-data-table/kup-data-table-helper';
-import { MockedRowsFactory } from './mocked-data';
+import {filterRows} from '../../../src/components/kup-data-table/kup-data-table-helper';
+import {MockedRowsFactory} from './mocked-data';
 
 const mockedRows = MockedRowsFactory();
 
@@ -9,132 +9,205 @@ mockedRowsWithEmptyValues[2].cells.FLD1.value = '';
 
 const displayedColumnsNames = ['FLD1', 'FLD2', 'FLD3', 'FLD4'];
 
-describe('it filters rows', () => {
-    it('filter without parameters', () => {
-        const filtered = filterRows();
+type FilterCompareFunction = (cellValue: string, filterText: string) => boolean;
 
-        expect(filtered).toEqual([]);
+describe('kup-data-table filters rows', () => {
+  it('filter without parameters', () => {
+    const filtered = filterRows();
+
+    expect(filtered).toEqual([]);
+  });
+
+  it('filter null rows', () => {
+    const filtered = filterRows(null);
+
+    expect(filtered).toEqual([]);
+  });
+
+  it('if no / null / empty filter, return rows as they are', () => {
+    let filtered = filterRows(mockedRows);
+
+    expect(filtered).toEqual(mockedRows);
+
+    filtered = filterRows(mockedRows, null);
+
+    expect(filtered).toEqual(mockedRows);
+
+    filtered = filterRows(mockedRows, {});
+
+    expect(filtered).toEqual(mockedRows);
+  });
+
+  it('returns empty array if no row matches', () => {
+    const filtered = filterRows(mockedRows, {
+      FLD1: 'clearly fake filter',
     });
 
-    it('filter null rows', () => {
-        const filtered = filterRows(null);
+    expect(filtered).not.toEqual(mockedRows);
 
-        expect(filtered).toEqual([]);
+    expect(filtered).toHaveLength(0);
+  });
+
+  it('filter on FLD1', () => {
+    const filtered = filterRows(mockedRows, {FLD1: 'fra'});
+
+    expect(filtered).not.toEqual(mockedRows);
+
+    expect(filtered).toHaveLength(5);
+  });
+
+  it('filter on FLD1 and FLD2', () => {
+    const filtered = filterRows(mockedRows, {FLD1: 'fra', FLD2: '12'});
+
+    expect(filtered).not.toEqual(mockedRows);
+
+    expect(filtered).toHaveLength(1);
+  });
+
+  it('global filter without columns', () => {
+    let filtered = filterRows(mockedRows, null, 'fra');
+
+    expect(filtered).toEqual(mockedRows);
+
+    filtered = filterRows(mockedRows, null, 'fra', null);
+
+    expect(filtered).toEqual(mockedRows);
+
+    filtered = filterRows(mockedRows, null, 'fra', []);
+
+    expect(filtered).toEqual(mockedRows);
+  });
+
+  it('global filter with columns', () => {
+    const filtered = filterRows(mockedRows, null, 'fra', [
+      'FLD1',
+      'FLD2',
+      'FLD3',
+      'FLD4',
+    ]);
+
+    expect(filtered).not.toEqual(mockedRows);
+
+    expect(filtered).toHaveLength(5);
+  });
+
+  it('global filter + column filter', () => {
+    const filtered = filterRows(
+      mockedRows,
+      {FLD1: 'cas', FLD2: '12'},
+      'fra',
+      displayedColumnsNames
+    );
+
+    expect(filtered).not.toEqual(mockedRows);
+
+    expect(filtered).toHaveLength(1);
+  });
+
+
+  //-------- Filter rows with expressions --------
+  describe(`with expression set to ''`, () => {
+
+    const columnToFilterOn = 'FLD1';
+
+    it('on column filter', () => {
+      const filtered = filterRows(
+        mockedRowsWithEmptyValues,
+        {[columnToFilterOn]: "''"}
+      );
+
+      expect(filtered).toHaveLength(3);
+      filtered.forEach(row => {
+        expect(row.cells[columnToFilterOn].value).toBe('');
+      })
+
     });
 
-    it('if no / null / empty filter, return rows as they are', () => {
-        let filtered = filterRows(mockedRows);
+    it('on global column filter', () => {
+      const filtered = filterRows(
+        mockedRowsWithEmptyValues,
+        {},
+        "''",
+        displayedColumnsNames
+      );
 
-        expect(filtered).toEqual(mockedRows);
+      // Expect to match the given number of items
+      expect(filtered).toHaveLength(5);
+      filtered.forEach(row => {
+        // Checks if there is at least a '' value in one of the displayed columns
+        expect(displayedColumnsNames.reduce((whiteSpaceFound, col) => row.cells[col] && row.cells[col].value === '' ? true : whiteSpaceFound, false))
+          .toBeTruthy();
+      })
 
-        filtered = filterRows(mockedRows, null);
+    });
+  });
 
-        expect(filtered).toEqual(mockedRows);
+  describe.each([
+    ['string%', 'FRA%', (cellValue, filterText) => cellValue.startsWith(filterText)],
+    ['%string', '%FRA', (cellValue, filterText) => cellValue.endsWith(filterText)],
+    ['%string%', '%FRA%', (cellValue, filterText) => cellValue.indexOf(filterText) >= 0],
+    ['string', 'FRA', (cellValue, filterText) => cellValue === filterText],
+  ])(`with expression set to '%s'`, (label: string, searchKey: string, compareFunction: FilterCompareFunction) => {
+    const columnToFilterOn = 'FLD1';
+    const filterText = searchKey.replace(/%/g, '');
 
-        filtered = filterRows(mockedRows, {});
+    it('on column filter', () => {
+      const filtered = filterRows(
+        mockedRowsWithEmptyValues,
+        {[columnToFilterOn]: `'${searchKey}'`}
+      );
 
-        expect(filtered).toEqual(mockedRows);
+      const filterProofRowsCount = mockedRowsWithEmptyValues.reduce(
+        (displayedRowsCount, {cells}) => displayedRowsCount += (compareFunction(cells[columnToFilterOn].value, filterText) ? 1 : 0),
+        0
+      );
+      expect(filtered).toHaveLength(filterProofRowsCount);
+
+      console.log("acolonna", filtered.length, filterProofRowsCount);
+
+      filtered.forEach(row => {
+        console.log(row.cells[columnToFilterOn].value);
+        expect(compareFunction(row.cells[columnToFilterOn].value, filterText)).toBeTruthy();
+      })
+
     });
 
-    it('returns empty array if no row matches', () => {
-        const filtered = filterRows(mockedRows, {
-            FLD1: 'clearly fake filter',
-        });
+    it('on global column filter', () => {
+      const filtered = filterRows(
+        mockedRowsWithEmptyValues,
+        {},
+        `'${searchKey}'`,
+        displayedColumnsNames
+      );
 
-        expect(filtered).not.toEqual(mockedRows);
+      const filterProofRowsCount = mockedRowsWithEmptyValues
+        .reduce((displayedRowsCount, {cells}) => {
+          let foundItem = false;
+          for (let i = 0; i < displayedColumnsNames.length && !foundItem; i++) {
+            foundItem = compareFunction(cells[displayedColumnsNames[i]].value, filterText);
+          }
 
-        expect(filtered).toHaveLength(0);
+          return displayedRowsCount += (foundItem ? 1 : 0);
+          },
+        0
+      );
+
+      // Expect to match the given number of items
+      expect(filtered).toHaveLength(filterProofRowsCount);
+
+      console.log("globali", filtered.length, filterProofRowsCount);
+
+      // Checks that each rendered row has at least one match with the filter.
+      filtered.forEach(({ cells }) => {
+        let foundItem = false;
+        for (let i = 0; i < displayedColumnsNames.length && !foundItem; i++) {
+          foundItem = compareFunction(cells[displayedColumnsNames[i]].value, filterText);
+        }
+        // Checks if there is at least a '' value in one of the displayed columns
+        expect(foundItem).toBeTruthy();
+      });
+
     });
 
-    it('filter on FLD1', () => {
-        const filtered = filterRows(mockedRows, { FLD1: 'fra' });
-
-        expect(filtered).not.toEqual(mockedRows);
-
-        expect(filtered).toHaveLength(5);
-    });
-
-    it('filter on FLD1 and FLD2', () => {
-        const filtered = filterRows(mockedRows, { FLD1: 'fra', FLD2: '12' });
-
-        expect(filtered).not.toEqual(mockedRows);
-
-        expect(filtered).toHaveLength(1);
-    });
-
-    it('global filter without columns', () => {
-        let filtered = filterRows(mockedRows, null, 'fra');
-
-        expect(filtered).toEqual(mockedRows);
-
-        filtered = filterRows(mockedRows, null, 'fra', null);
-
-        expect(filtered).toEqual(mockedRows);
-
-        filtered = filterRows(mockedRows, null, 'fra', []);
-
-        expect(filtered).toEqual(mockedRows);
-    });
-
-    it('global filter with columns', () => {
-        const filtered = filterRows(mockedRows, null, 'fra', [
-            'FLD1',
-            'FLD2',
-            'FLD3',
-            'FLD4',
-        ]);
-
-        expect(filtered).not.toEqual(mockedRows);
-
-        expect(filtered).toHaveLength(5);
-    });
-
-    it('global filter + column filter', () => {
-        const filtered = filterRows(
-            mockedRows,
-            { FLD1: 'cas', FLD2: '12' },
-            'fra',
-          displayedColumnsNames
-        );
-
-        expect(filtered).not.toEqual(mockedRows);
-
-        expect(filtered).toHaveLength(1);
-    });
-
-    describe(`with filter set to ''`, () => {
-
-        const columnToFilterOn = 'FLD1';
-
-        it('on column filter', () => {
-            const filtered = filterRows(
-              mockedRowsWithEmptyValues,
-              {[columnToFilterOn]: "''"}
-            );
-
-            expect(filtered).toHaveLength(3);
-            filtered.forEach(row => {
-                expect(row.cells[columnToFilterOn].value).toBe('');
-            })
-
-        });
-
-        it('on global column filter', () => {
-            const filtered = filterRows(
-              mockedRowsWithEmptyValues,
-              {},
-              "''",
-              displayedColumnsNames
-            );
-
-            // Expect to match the given number of items
-            expect(filtered).toHaveLength(5);
-            filtered.forEach(row => {
-                // Checks if there is at least a '' value in one of the displayed columns
-                expect(displayedColumnsNames.reduce((whiteSpaceFound, col) => row.cells[col] && row.cells[col].value === '' ? true : whiteSpaceFound, false))
-                  .toBeTruthy();
-            })
-
-        });
-    });
+  });
 });
