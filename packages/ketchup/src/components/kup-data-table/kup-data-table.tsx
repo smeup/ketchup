128d5fe6ec6f6d75e8c7edffa9b64b8503fabe45
foscili-smeup <lucafoscili@icloud.com>
2019-10-30 12:16:05 +0100

    Removed sort from sticky header

--- packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
+++ packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
@@ -1476,23 +1476,6 @@ export class KupDataTable {
         const hasCustomColumnsWidth = this.columnsWidth.length > 0;
 
         const dataColumns = this.getVisibleColumns().map((column) => {
-            // sort
-            let sort = null;
-            if (this.sortEnabled) {
-                sort = (
-                    <span class="column-sort">
-                        <span
-                            role="button"
-                            aria-label="Sort column" // TODO
-                            class={'mdi ' + this.getSortIcon(column.name)}
-                            onClick={(e: MouseEvent) =>
-                                this.onColumnSort(e, column.name)
-                            }
-                        />
-                    </span>
-                );
-            }
-
             let thStyle = null;
             if (hasCustomColumnsWidth) {
                 for (let i = 0; i < this.columnsWidth.length; i++) {
@@ -1520,7 +1503,6 @@ export class KupDataTable {
             return (
                 <th-sticky class={columnClass} style={thStyle}>
                     <span class="column-title">{column.title}</span>
-                    {sort}
                 </th-sticky>
             );
         });
