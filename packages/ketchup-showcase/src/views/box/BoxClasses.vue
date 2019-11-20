84f89d2e440a97b230acb2ad993ffcf3d59ca7c6
foscili-smeup <lucafoscili@icloud.com>
2019-11-20 17:38:12 +0100

    Without padding in showcase

--- packages/ketchup-showcase/src/views/box/BoxClasses.vue
+++ packages/ketchup-showcase/src/views/box/BoxClasses.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>no-shadow</h3>
     <kup-box :data.prop="basicData" class="no-shadow"></kup-box>
-    <h3>with-padding</h3>
+    <h3>without-padding</h3>
     <kup-box :data.prop="basicData" class="without-padding"></kup-box>
   </div>
 </template>
