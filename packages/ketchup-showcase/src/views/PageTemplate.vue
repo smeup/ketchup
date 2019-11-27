118b5100eb47cca141ab0bfc54eb0f2f3ef0d882
foscili-smeup <lucafoscili@icloud.com>
2019-11-27 08:59:17 +0100

    Added new page template and called in box

---
+++ packages/ketchup-showcase/src/views/PageTemplate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="page-wrapper">
+    <div id="page-content">
+      <div v-for="(item, index) in titles" :key="item" class="section">
+        <h1>{{ item }}</h1>
+        <slot :name="'' + index"> </slot>
+      </div>
+    </div>
+    <div id="page-nav">
+      <a
+        v-for="item in titles"
+        :key="item"
+        onclick="scrollToSmoothly();"
+        class="page-nav-element"
+        >{{ item }}</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default { props: ['titles'] };
+</script>
