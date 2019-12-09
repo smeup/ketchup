7bbd13469a7f18c781570688ffeac59535183752
Francesco Bonacini <f.bonacini@dreamonkey.com>
2019-12-09 14:17:16 +0100

    [kup-autocomplete] created base for autocomplete: tests and some declarations.

---
+++ packages/ketchup/src/components/kup-autocomplete/kup-autocomplete-declarations.tsx
@@ -0,0 +1,16 @@
+export enum AutocompleteDisplayMode {
+  CODE = 'code',
+  DESCRIPTION = 'label',
+  DESCRIPTION_AND_CODE = 'both',
+}
+
+export enum AutocompleteSortBy {
+  CODE = 'code',
+  DESCRIPTION = 'description',
+}
+
+export enum AutocompleteSortOrder {
+  DESCENDING = 'descending',
+  INCREASING = 'increasing',
+}
+
