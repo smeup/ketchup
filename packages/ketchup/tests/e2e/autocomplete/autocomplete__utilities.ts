61080e8e352c6ef84d4057f0f09958adabc7a51e
Francesco Bonacini <f.bonacini@dreamonkey.com>
2019-12-23 11:16:29 +0100

    [kup-autocomplete] tests for icons.

--- packages/ketchup/tests/e2e/autocomplete/autocomplete__utilities.ts
+++ packages/ketchup/tests/e2e/autocomplete/autocomplete__utilities.ts
@@ -1,4 +1,4 @@
-import {E2EPage, E2EElement} from '@stencil/core/testing';
+import {E2EElement, E2EPage} from '@stencil/core/testing';
 
 export async function getAutocompleteInputField(page: E2EPage): Promise<E2EElement> {
   //const kupInput = await page.find('kup-autocomplete >>> kup-text-input >>> input[type="text"]');
@@ -8,4 +8,8 @@ export async function getAutocompleteInputField(page: E2EPage): Promise<E2EEleme
 
 export async function getAutocompleteMenuInstance(page: E2EPage): Promise<E2EElement> {
   return page.find('kup-autocomplete >>> kup-menu');
+}
+
+export async function getAutocompleteClearIcon(page: E2EPage): Promise<E2EElement> {
+  return (page.evaluateHandle(`document.querySelector("kup-autocomplete").shadowRoot.querySelector("kup-text-input").shadowRoot.querySelector("button")`)) as unknown as E2EElement;
 }
