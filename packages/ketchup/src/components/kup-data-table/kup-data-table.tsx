9d1637e9485769e0935bf6c2152bb8daa70df42e
Chiara Zambelli <chiara.zambelli@smeup.com>
2020-02-17 17:38:59 +0100

    checkbox filterable in table

--- packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
+++ packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
@@ -58,7 +58,6 @@ import {
     isBar,
     isChart,
     isButton,
-    isCheckbox,
     isIcon,
     isImage,
     isLink,
@@ -67,6 +66,7 @@ import {
     isRadio,
     isVoCodver,
     isStringObject,
+    isCheckbox,
 } from '../../utils/object-utils';
 
 @Component({
@@ -1263,7 +1263,10 @@ export class KupDataTable {
                     ? this.filters[column.name]
                     : '';
 
-            if (this.showFilters && isStringObject(column.obj)) {
+            if (
+                this.showFilters &&
+                (isStringObject(column.obj) || isCheckbox(column.obj))
+            ) {
                 // When showing filters, displays input box to update them.
                 filter = (
                     <div>
