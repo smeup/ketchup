6021055005a001f3a82461ba1d322b6fba9d8312
Francesco Bonacini <f.bonacini@dreamonkey.com>
2020-03-04 12:31:20 +0100

    [kup-data-table] finished the test to verify the correct work of the fixed cell on both column and row direction.

--- packages/ketchup/tests/e2e/data-table/data-table-fixed-rows-columns.e2e.ts
+++ packages/ketchup/tests/e2e/data-table/data-table-fixed-rows-columns.e2e.ts
@@ -3,6 +3,7 @@ import {newE2EPage} from '@stencil/core/testing';
 import {createData} from './mocked-data';
 
 import {
+    Column,
     FixedCellsClasses,
     FixedCellsCSSVarsBase
 } from '../../../src/components/kup-data-table/kup-data-table-declarations';
@@ -13,6 +14,10 @@ import {getElementClientRect} from '../E2eTestUtilities';
  * Page element created before each test by the beforeEach function
  */
 let page;
+/**
+ * The selector for getting the last cell of the last row of the data table
+ */
+const lastCellSelector = 'kup-data-table >>> ' + 'tbody > tr:last-of-type > td:last-of-type';
 /**
  * The number of rows generated for the data used in these tests
  */
@@ -29,7 +34,7 @@ const testData = createData(12, testDataRows);
 /**
  * The custom fixed height of the table
  */
-const tableCustomHeight = 200;
+const tableCustomHeight = 150;
 /**
  * The CSS var name which enforces the height of the working table area.
  */
@@ -44,8 +49,17 @@ kup-data-table {
 }
 `;
 
+//---- TYPES ----
+
+type DataTableCellIndexesCollection = Array<{col: number, row: number}>;
+
+//---- Helper functions ----
 function getPropDirectionFromCurrent(direction: string): string {
     return 'fixed' + direction.slice(0,1).toUpperCase() + direction.slice(1, direction.length);
+}
+
+function getDataTableVisibleColumns(columns: Column[]) {
+    return columns.filter(col => col.visible === undefined || col.visible).length;
 }
 
 it(`kup-data-table table area can be limited by using the CSS property ${tableCustomHeightCSSVarName}`, async () => {
@@ -112,7 +126,7 @@ describe.each([
         [3],
     ])(`set to a value > 0 and < columns.number, sticky position the correct number (currently %i) of ${currentDirection} starting from the ${startDirection} of the table`, async (fixedCount: number) => {
         // Gets basic information
-        const visibleColumnsCount = testData.columns.filter(col => col.visible === undefined || col.visible).length;
+        const visibleColumnsCount = getDataTableVisibleColumns(testData.columns);
 
         // Sets height and width correctly to force a scrollable table
         await page.addStyleTag({content: tableCustomHeightStyle});
@@ -123,12 +137,12 @@ describe.each([
         // Sets height correctly to force a scrollable table
         await page.addStyleTag({content: tableCustomHeightStyle});
         await page.waitForChanges();
-        const allTbodyCells = await page.findAll('kup-data-table >>> td');
+        const allTbodyCells = await page.findAll('kup-data-table >>> tbody td');
         const allHeaderCells = await page.findAll('kup-data-table >>> th');
         const allCells = allTbodyCells.concat(allHeaderCells);
 
         // Cells to check the position
-        const cellsToCheck: Array<{col: number, row: number}> = [];
+        const cellsToCheck: DataTableCellIndexesCollection = [];
 
         // Checks that only the correct cells have the necessary fixed related classes
         // Controls both the thead and the tbody
@@ -167,7 +181,6 @@ describe.each([
 
         // Gets the client rect of the last cell of the table
         // It will be used to be sure that non fixed cells have been scrolled
-        const lastCellSelector = 'kup-data-table >>> ' + 'tbody > tr:last-of-type > td:last-of-type';
         const lastCellClientRectBefore = await getElementClientRect(page, lastCellSelector);
 
         // scrolls the table in the correct direction
@@ -213,9 +226,71 @@ describe.each([
     });
 });
 
-it.skip(`kup-data-table with fixed rows and columns both > 0 have a matrix of cells with both fixed behaviors`, async () => {
-    // TODO check if there are other things to test
+it(`kup-data-table with fixed rows and columns both > 0 have a matrix of cells with both fixed behaviors`, async () => {
+    // Constants
     const fixedRows = 3;
     const fixedColumns = 2;
-    expect(false).toBeTruthy();
+    const visibleColumnsCount = getDataTableVisibleColumns(testData.columns);
+
+    // Test setup
+    const page = await newE2EPage();
+    await page.setContent(`<kup-data-table></kup-data-table>`);
+    const tableElement = await page.find('kup-data-table');
+    await tableElement.setProperty('data', testData);
+    await tableElement.setProperty('rowsPerPage', testDataRows);
+    await tableElement.setProperty('fixedColumns', fixedColumns);
+    await tableElement.setProperty('fixedRows', fixedRows);
+    // Style must be injected later to be sure it is applied
+    await page.addStyleTag({content: tableCustomHeightStyle});
+    await page.waitForChanges();
+
+    const tbodyCells = await page.findAll('kup-data-table >>> tbody td');
+
+    // Gets all cells which must be fixed in both directions
+    let currentCell;
+    const bothDirectionFixedCells: DataTableCellIndexesCollection = [];
+    const initialClientRect: ClientRect[] = [];
+    for (let rowIndex = 0; rowIndex < fixedRows; rowIndex++) {
+        for (let colIndex = 0; colIndex < fixedColumns; colIndex++) {
+            currentCell = tbodyCells[rowIndex * visibleColumnsCount + colIndex];
+            expect(currentCell).toHaveClass(FixedCellsClasses.rows);
+            expect(currentCell).toHaveClass(FixedCellsClasses.columns);
+
+            bothDirectionFixedCells.push({
+                col: colIndex,
+                row: rowIndex
+            });
+
+            initialClientRect.push(
+                await getElementClientRect(page, `kup-data-table >>> tbody > tr:nth-of-type(${rowIndex + 1}) > td:nth-of-type(${colIndex + 1})`)
+            );
+        }
+    }
+
+    // Before scrolling the table, we save the ClientRect of the last cell to be sure that the table has been scrolled.
+    const lastCellClientRectBefore = await getElementClientRect(page, lastCellSelector);
+
+    // Scroll into view the last cells of the last row
+    await page.evaluate(() => {
+        document.querySelector('kup-data-table').shadowRoot
+            .querySelector(
+                    'tbody > tr:last-of-type > td:last-of-type'
+            ).scrollIntoView();
+    });
+    await page.waitForChanges();
+
+    // Compares ClientRect from before and after the scroll
+    let currentCellClientRect: ClientRect;
+    for (let i = 0; i < bothDirectionFixedCells.length; i++) {
+        currentCellClientRect = await getElementClientRect(page, `kup-data-table >>> tbody > tr:nth-of-type(${bothDirectionFixedCells[i].row + 1}) > td:nth-of-type(${bothDirectionFixedCells[i].col + 1})`)
+
+        expect(currentCellClientRect.top).toEqual(initialClientRect[i].top);
+        expect(currentCellClientRect.left).toEqual(initialClientRect[i].left);
+    }
+
+    // Checks the last cell to be sure the table has been scrolled
+    const lastCellClientRectAfter = await getElementClientRect(page, lastCellSelector);
+
+    expect(lastCellClientRectAfter.top).toBeLessThan(lastCellClientRectBefore.top);
+    expect(lastCellClientRectAfter.left).toBeLessThan(lastCellClientRectBefore.left);
 });
