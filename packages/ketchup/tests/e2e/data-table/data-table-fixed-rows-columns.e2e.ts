832802a1308eccd8b78091dc43da91ce3f41b513
Francesco Bonacini <f.bonacini@dreamonkey.com>
2020-03-04 11:22:23 +0100

    [kup-data-table] finished the test to verify the correct work of the fixed row/column.

--- packages/ketchup/tests/e2e/data-table/data-table-fixed-rows-columns.e2e.ts
+++ packages/ketchup/tests/e2e/data-table/data-table-fixed-rows-columns.e2e.ts
@@ -14,17 +14,22 @@ import {getElementClientRect} from '../E2eTestUtilities';
  */
 let page;
 /**
+ * The number of rows generated for the data used in these tests
+ */
+const testDataRows = 10;
+/**
  * tableElement fetched before each test by the beforeEach function
  */
 let tableElement;
 /**
  * Generated data used to populate the data table for these tests
+ * TODO move row size to a variable
  */
-const testData = createData(12, 30);
+const testData = createData(12, testDataRows);
 /**
  * The custom fixed height of the table
  */
-const tableCustomHeight = 300;
+const tableCustomHeight = 200;
 /**
  * The CSS var name which enforces the height of the working table area.
  */
@@ -68,6 +73,7 @@ describe.each([
         await page.setContent(`<kup-data-table></kup-data-table>`);
         tableElement = await page.find('kup-data-table');
         await tableElement.setProperty('data', testData);
+        await tableElement.setProperty('rowsPerPage', testDataRows);
         await page.waitForChanges();
     });
 
@@ -107,42 +113,89 @@ describe.each([
     ])(`set to a value > 0 and < columns.number, sticky position the correct number (currently %i) of ${currentDirection} starting from the ${startDirection} of the table`, async (fixedCount: number) => {
         // Gets basic information
         const visibleColumnsCount = testData.columns.filter(col => col.visible === undefined || col.visible).length;
-        const rowsCount = testData.rows.length;
 
-        // Sets height correctly to force a scrollable table
+        // Sets height and width correctly to force a scrollable table
         await page.addStyleTag({content: tableCustomHeightStyle});
 
         // Set the fixed direction elements
         const propDirection = getPropDirectionFromCurrent(currentDirection);
         await tableElement.setProperty(propDirection, fixedCount);
+        // Sets height correctly to force a scrollable table
+        await page.addStyleTag({content: tableCustomHeightStyle});
         await page.waitForChanges();
         const allTbodyCells = await page.findAll('kup-data-table >>> td');
         const allHeaderCells = await page.findAll('kup-data-table >>> th');
         const allCells = allTbodyCells.concat(allHeaderCells);
 
+        // Cells to check the position
+        const cellsToCheck: Array<{col: number, row: number}> = [];
+
         // Checks that only the correct cells have the necessary fixed related classes
         // Controls both the thead and the tbody
-        // For each row
-        for (let rowIndex = 0; rowIndex < allCells.length / rowsCount; rowIndex++) {
+        // For each row + the head
+        for (let rowIndex = 0; rowIndex < testDataRows + 1; rowIndex++) {
             //For each column
             for (let colIndex = 0; colIndex < visibleColumnsCount; colIndex++) {
                 // Gets the current cell
-                const currentCell = allTbodyCells[rowIndex * visibleColumnsCount + colIndex];
+                const currentCell = allCells[rowIndex * visibleColumnsCount + colIndex];
                 // Checks classes for the rows or for the columns.
                 if ((currentDirection === 'rows' ? rowIndex : colIndex) < fixedCount) {
                     expect(currentCell).toHaveClass(FixedCellsClasses[currentDirection]);
+
+                    // Stores the fact that this cell needs to be checked later
+                    // avoid thead
+                    if (rowIndex < testDataRows) {
+                        cellsToCheck.push({
+                            col: colIndex,
+                            row: rowIndex
+                        });
+                    }
                 } else {
                     expect(currentCell).not.toHaveClass(FixedCellsClasses[currentDirection]);
                 }
             }
         }
 
-        // gets rect positions of all fixed cells inside the table and stores them
-        // scrolls the table in the correct direction
-        // gets the fixed cells again and controls if they have retained the same positions
-        // controls that other cells have been scrolled
+        // Gets rect positions of all fixed cells inside the table and stores them
+        // Control on these cells will happen later
+        const initialClientRect: ClientRect[] = [];
+        for (let i = 0; i < cellsToCheck.length; i++) {
+            initialClientRect.push(
+                await getElementClientRect(page, `kup-data-table >>> tbody > tr:nth-of-type(${cellsToCheck[i].row + 1}) > td:nth-of-type(${cellsToCheck[i].col + 1})`)
+            );
+        }
 
-    });
+        // Gets the client rect of the last cell of the table
+        // It will be used to be sure that non fixed cells have been scrolled
+        const lastCellSelector = 'kup-data-table >>> ' + 'tbody > tr:last-of-type > td:last-of-type';
+        const lastCellClientRectBefore = await getElementClientRect(page, lastCellSelector);
+
+        // scrolls the table in the correct direction
+        await page.evaluate((direction) => {
+            document.querySelector('kup-data-table').shadowRoot
+                .querySelector(
+                    // Checks in which direction we must scroll
+                    direction === 'rows' ?
+                        'tbody > tr:last-of-type > td:first-of-type' : // fixed rows -> we use the first cell of the last row
+                        'thead th:last-of-type' // fixed-columns -> we use last thead cell
+                ).scrollIntoView();
+        }, currentDirection);
+        await page.waitForChanges();
+
+        // gets the fixed cells again and controls if they have retained the same positions
+        let currentCellClientRect: ClientRect;
+        for (let i = 0; i < cellsToCheck.length; i++) {
+            currentCellClientRect = await getElementClientRect(
+                page,
+                `kup-data-table >>> tbody > tr:nth-of-type(${cellsToCheck[i].row + 1}) > td:nth-of-type(${cellsToCheck[i].col + 1})`
+            );
+            expect(currentCellClientRect[startDirection]).toEqual(initialClientRect[i][startDirection])
+        }
+
+        // controls that other cells have been scrolled
+        const lastCellClientRectAfter = await getElementClientRect(page, lastCellSelector);
+        expect(lastCellClientRectAfter[startDirection]).toBeLessThan(lastCellClientRectBefore[startDirection]);
+    }, 30000);
 
     it(`and with grouping activated does not position sticky any ${currentDirection}`, async () => {
         const propDirection = getPropDirectionFromCurrent(currentDirection);
