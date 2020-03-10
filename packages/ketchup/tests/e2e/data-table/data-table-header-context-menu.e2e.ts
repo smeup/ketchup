e897b96a9520976e63fa9fd3f190e33431db7f92
Francesco Bonacini <f.bonacini@dreamonkey.com>
2020-03-10 11:51:01 +0100

    (feat. custom menu on table header): tests: completed automated tests for the header menu.

--- packages/ketchup/tests/e2e/data-table/data-table-header-context-menu.e2e.ts
+++ packages/ketchup/tests/e2e/data-table/data-table-header-context-menu.e2e.ts
@@ -15,7 +15,7 @@ describe('kup-data-table thead column context menu', () => {
     beforeEach(async () => {
         // Sets data
         page = await newE2EPage();
-        await page.setContent('<kup-data-table></kup-data-table>');
+        await page.setContent('<span id="external-span">external cell content</span><kup-data-table></kup-data-table><div id="external-div">external div content</div>');
         tableElement = await page.find('kup-data-table');
         await tableElement.setProperty('data', staticData);
         await page.waitForChanges();
@@ -29,8 +29,6 @@ describe('kup-data-table thead column context menu', () => {
                 y: clientRect.top + clientRect.height / 2
             };
         });
-
-        console.log("dentro la callback", coordinates);
 
         await page.mouse.click(coordinates.x, coordinates.y, {button: 'right'});
         await page.waitForChanges();
@@ -48,22 +46,68 @@ describe('kup-data-table thead column context menu', () => {
         }
     });
 
-    it('once opened, is closed by left click anywhere but from within the menu itself', async () => {
-        // Checks that the first menu is open
-        const openMenu = await page.find('kup-data-table >>> thead .column-menu');
-        expect(openMenu).toHaveClass('open');
+    describe('once opened,', () => {
+        const firstMenuSelector = 'thead > tr > th:first-of-type .column-menu';
 
-        await page.evaluate( () => {
-            (document.querySelector('kup-data-table').shadowRoot.querySelector('tbody > tr:first-of-type > td:first-of-type') as HTMLTableCellElement).click();
-            return true;
+        it('is not automatically closed by a left click from within the open menu', async () => {
+            // Checks that the first menu is open
+            const openMenuBefore = await page.find(`kup-data-table >>> ${firstMenuSelector}`);
+            expect(openMenuBefore).toHaveClass('open');
+
+            await page.evaluate((menuSelector) => {
+                (document.querySelector('kup-data-table').shadowRoot.querySelector(menuSelector) as HTMLDivElement).click();
+                return true;
+            }, firstMenuSelector);
+
+            await page.waitForChanges();
+
+            const openMenuAfter = await page.find(`kup-data-table >>> ${firstMenuSelector}`);
+            expect(openMenuAfter).toHaveClass('open');
         });
 
-        await  page.waitForChanges();
+        describe('is closed by a left click', () => {
+            it.each([
+                ['thead > tr:first-of-type > th:first-of-type'],
+                ['thead > tr:first-of-type > th:last-of-type'],
+                ['tbody > tr:first-of-type > td:first-of-type'],
+                ['tbody > tr:last-of-type > td:last-of-type'],
+            ])('anywhere from within the table but not from within the menu itself (selector: %s)', async (currentSelector) => {
+                // Checks that the first menu is open
+                const openMenu = await page.find('kup-data-table >>> thead .column-menu');
+                expect(openMenu).toHaveClass('open');
 
-        // Checks that each menu now is closed
-        menuElements = await page.findAll('kup-data-table >>> thead .column-menu');
-        for (let i = 0; i < menuElements.length; i++) {
-            expect(menuElements[i]).toHaveClass('closed');
-        }
+                await page.evaluate( (currentClickSelector) => {
+                    (document.querySelector('kup-data-table').shadowRoot.querySelector(currentClickSelector) as HTMLTableCellElement).click();
+                    return true;
+                }, currentSelector);
+
+                await  page.waitForChanges();
+
+                // Checks that each menu now is closed
+                menuElements = await page.findAll('kup-data-table >>> thead .column-menu');
+                for (let i = 0; i < menuElements.length; i++) {
+                    expect(menuElements[i]).toHaveClass('closed');
+                }
+            });
+
+            it.each([
+                ['span element placed before the table', '#external-span'],
+                ['div element placed after the table', '#external-div']
+            ])('from anywhere outside the kup-data-table (%s)', async(description, selector) => {
+                // Checks that the first menu is open
+                const openMenuBefore = await page.find(`kup-data-table >>> ${firstMenuSelector}`);
+                expect(openMenuBefore).toHaveClass('open');
+
+                await page.evaluate( (currentClickSelector) => {
+                    (document.querySelector(currentClickSelector) as HTMLElement).click();
+                    return true;
+                }, selector);
+
+                await page.waitForChanges();
+
+                const openMenuAfter = await page.find(`kup-data-table >>> ${firstMenuSelector}`);
+                expect(openMenuAfter).toHaveClass('closed');
+            });
+        });
     });
 });
