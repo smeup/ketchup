298bb10b102b53284c263546c0e8e047c03bae3e
Carina <carinagentiana.pasere@smeup.com>
2020-05-12 17:41:01 +0200

    fix IT kup-tree

--- packages/ketchup/tests/e2e/tree/tree__selectors.ts
+++ packages/ketchup/tests/e2e/tree/tree__selectors.ts
@@ -4,14 +4,14 @@ const TableRows = Tree + ' > tbody > tr';
 const TreeNodeCell = 'td:nth-of-type(1)';
 
 export const KupTreeSelectors = {
-  TreeHeader,
-  TreeHeaderCells: TreeHeader + ' th',
-  TableRows,
-  TreeNodeCell,
-  OnlyTreeNodeCells: TableRows + ' > ' + TreeNodeCell,
-  OptionElement: 'span.options',
-  simple: {
-    CellIcon: '.kup-tree__icon',
-    TreeNodeExpander: '.kup-tree__node__expander',
-  }
+    TreeHeader,
+    TreeHeaderCells: TreeHeader + ' th',
+    TableRows,
+    TreeNodeCell,
+    OnlyTreeNodeCells: TableRows + ' > ' + TreeNodeCell,
+    OptionElement: 'kup-button',
+    simple: {
+        CellIcon: '.kup-tree__icon',
+        TreeNodeExpander: '.kup-tree__node__expander',
+    },
 };
