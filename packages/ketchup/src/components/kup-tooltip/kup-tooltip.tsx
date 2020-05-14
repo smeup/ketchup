d2b63fcf7213e6f5206067064af4c34366c32b98
Francesco Verzeletti <francesco.verzeletti@smeup.com>
2020-05-14 17:14:43 +0200

    Modificato timeout di apertura tooltip

--- packages/ketchup/src/components/kup-tooltip/kup-tooltip.tsx
+++ packages/ketchup/src/components/kup-tooltip/kup-tooltip.tsx
@@ -222,7 +222,7 @@ export class KupTooltip {
                 this.visible = true;
 
                 this.kupTooltipLoadData.emit();
-            }, 400);
+            }, 1500);
         }
     }
 
