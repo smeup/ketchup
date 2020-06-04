ce39075123c66733553eb60942094dd4aa1761c2
foscili-smeup <lucafoscili@icloud.com>
2020-06-04 09:06:33 +0200

    Showcase update.

---
+++ packages/ketchup-showcase/src/views/advanced/card/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <comp :giturl="giturl" :headtitle="headtitle" :titles="titles">
+      <template v-slot:0>
+        <card-demo></card-demo>
+      </template>
+    </comp>
+  </div>
+</template>
+
+<script>
+import CardDemo from '@/views/advanced/card/examples/CardDemo';
+import Comp from '@/views/advanced/page/templates/Comp';
+export default {
+  components: {
+    CardDemo,
+    Comp,
+  },
+  data() {
+    return {
+      giturl:
+        'https://github.com/smeup/ketchup/tree/develop/packages/ketchup/src/components/kup-card',
+      headtitle: 'Card',
+      titles: ['Playground'],
+    };
+  },
+};
+</script>
---
+++ packages/ketchup-showcase/src/views/advanced/card/examples/CardDemo.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <demo :demoTabs="demoTabs" :demoComp="demoComp" :demoProps="demoProps" :demoData="demoData"></demo>
+  </div>
+</template>
+
+<script>
+import Demo from '@/views/advanced/page/templates/Demo';
+
+export default {
+  components: {
+    Demo,
+  },
+  name: 'CardDemo',
+  data() {
+    return {
+      demoTabs: [
+        {
+          text: 'Props',
+          icon: '',
+          active: true,
+        },
+        {
+          text: 'HTML',
+          icon: '',
+          active: false,
+        },
+        {
+          text: 'JSON',
+          icon: '',
+          active: false,
+        },
+        {
+          text: 'CSS',
+          icon: '',
+          active: false,
+        },
+      ],
+      demoComp: '<kup-card layout="1" id="demo-component"></kup-card>',
+      demoProps: [
+        {
+          prop: 'customStyle',
+          description:
+            'Sets a custom style for the component by feeding this string into a <style> tag.',
+          type: 'string',
+          default: 'null',
+          try: 'css',
+        },
+        {
+          prop: 'data',
+          description: 'The actual data of the card.',
+          type: 'ComponentCardElement',
+          default: 'undefined',
+          try: 'json',
+        },
+        {
+          prop: 'isMenu',
+          description: 'Defines whether the card is a menu or not.',
+          type: 'boolean',
+          default: 'false',
+          try: 'switch',
+        },
+        {
+          prop: 'layout',
+          description: 'Sets the layout of the card.',
+          type: 'number',
+          default: '1',
+          try: 'field',
+        },
+        {
+          prop: 'menuVisible',
+          description:
+            "Sets the status of the menu, when false it's hidden otherwise it's visible.",
+          type: 'boolean',
+          default: 'false',
+          try: 'switch',
+        },
+        {
+          prop: 'sizeX',
+          description:
+            'The width of the card, defaults to 100%. Accepts any valid CSS format (px, %, vh, etc.).',
+          type: 'string',
+          default: '100%',
+          try: 'field',
+        },
+        {
+          prop: 'sizeY',
+          description:
+            'The height of the card, defaults to 100%. Accepts any valid CSS format (px, %, vh, etc.).',
+          type: 'string',
+          default: '100%',
+          try: 'field',
+        },
+      ],
+      demoData: {
+        data: {
+          button1: { icon: 'play', flat: true },
+          color1: 'green',
+          color2: 'var(--kup-title-background-color)',
+          emphText1: 'Ketch.UP',
+          emphText2: 'High',
+          emphText3: '12/31/2020',
+          image1: { resource: 'widgets' },
+          image2: {
+            resource:
+              'https://ketchup.smeup.com/ketchup-showcase/images/arthas.jpg',
+          },
+          progressBar1: { value: 75, centeredLabel: false },
+          text1: 'This is the text of this TODO',
+          text2: 'Priority',
+          text3: 'Deadline',
+        },
+      },
+    };
+  },
+};
+</script>
