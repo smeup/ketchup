1f938bb3416bc2d27183f4b4b521c0f0c1dc80bf
foscili-smeup <lucafoscili@icloud.com>
2020-06-09 09:14:04 +0200

    3rd material layout and minor fixes.

--- packages/ketchup/src/components/kup-card/material/kup-card-material.tsx
+++ packages/ketchup/src/components/kup-card/material/kup-card-material.tsx
@@ -69,16 +69,16 @@ export function create2(layout: number, data: ComponentCardElement) {
         button1 = <kup-button id="button1" {...data['button1']}></kup-button>;
     }
     if (data.button2) {
-        button2 = <kup-button id="button1" {...data['button2']}></kup-button>;
+        button2 = <kup-button id="button2" {...data['button2']}></kup-button>;
     }
     if (data.button3) {
-        button3 = <kup-button id="button1" {...data['button3']}></kup-button>;
+        button3 = <kup-button id="button3" {...data['button3']}></kup-button>;
     }
     if (data.button4) {
-        button4 = <kup-button id="button1" {...data['button4']}></kup-button>;
+        button4 = <kup-button id="button4" {...data['button4']}></kup-button>;
     }
     if (data.button5) {
-        button5 = <kup-button id="button1" {...data['button5']}></kup-button>;
+        button5 = <kup-button id="button5" {...data['button5']}></kup-button>;
     }
     return (
         <div class={componentClass}>
@@ -99,6 +99,69 @@ export function create2(layout: number, data: ComponentCardElement) {
                 </div>
             </div>
             <div class="section-4">
+                {button1}
+                {button2}
+                {button3}
+                {button4}
+                {button5}
+            </div>
+        </div>
+    );
+}
+
+export function create3(layout: number, data: ComponentCardElement) {
+    let componentClass = 'material-layout-' + layout;
+    var button1: KupButton = undefined;
+    var button2: KupButton = undefined;
+    var button3: KupButton = undefined;
+    var button4: KupButton = undefined;
+    var button5: KupButton = undefined;
+    let image1: KupImage = (
+        <kup-image
+            customStyle="img {object-fit: cover;}"
+            id="image1"
+            {...data['image1']}
+        ></kup-image>
+    );
+    if (data.button1) {
+        componentClass += ' has-actions';
+        button1 = <kup-button id="button1" {...data['button1']}></kup-button>;
+    }
+    if (data.button2) {
+        button2 = <kup-button id="button2" {...data['button2']}></kup-button>;
+    }
+    if (data.button3) {
+        button3 = <kup-button id="button3" {...data['button3']}></kup-button>;
+    }
+    if (data.button4) {
+        button4 = <kup-button id="button4" {...data['button4']}></kup-button>;
+    }
+    if (data.button5) {
+        button5 = <kup-button id="button5" {...data['button5']}></kup-button>;
+    }
+    return (
+        <div class={componentClass}>
+            <div class="mdc-ripple-surface">
+                <div class="section-1">
+                    <div class="media">
+                        {image1}
+                        <div class="text-on-media">
+                            <div class="sub-1 title">
+                                <div>{data['text1']}</div>
+                            </div>
+                            <div class="sub-1 subtitle">
+                                <div>{data['text2']}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-2">
+                    <div class="sub-2 description">
+                        <div>{data['text3']}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-3">
                 {button1}
                 {button2}
                 {button3}
