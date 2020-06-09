1fbb356c3ea0a5ae18d5dbd5b335bfb7835d55a9
foscili-smeup <lucafoscili@icloud.com>
2020-06-09 11:29:39 +0200

    Showcase draft.

--- packages/ketchup-showcase/src/views/advanced/card/Card.vue
+++ packages/ketchup-showcase/src/views/advanced/card/Card.vue
@@ -4,16 +4,26 @@
       <template v-slot:0>
         <card-demo></card-demo>
       </template>
+      <template v-slot:1>
+        <card-basic></card-basic>
+      </template>
+      <template v-slot:2>
+        <card-styling></card-styling>
+      </template>
     </comp>
   </div>
 </template>
 
 <script>
 import CardDemo from '@/views/advanced/card/examples/CardDemo';
+import CardBasic from '@/views/advanced/card/examples/CardBasic';
+import CardStyling from '@/views/advanced/card/examples/CardStyling';
 import Comp from '@/views/advanced/page/templates/Comp';
 export default {
   components: {
     CardDemo,
+    CardBasic,
+    CardStyling,
     Comp,
   },
   data() {
@@ -21,7 +31,7 @@ export default {
       giturl:
         'https://github.com/smeup/ketchup/tree/develop/packages/ketchup/src/components/kup-card',
       headtitle: 'Card',
-      titles: ['Playground'],
+      titles: ['Playground', 'Basic usage', 'Styling'],
     };
   },
 };
---
+++ packages/ketchup-showcase/src/views/advanced/card/examples/CardBasic.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="demo-wrapper">
+      <p>Cards are surfaces containing informations or actions regarding a single subject.</p>
+      <p>What makes the cards really useful is the concept of a premade layout which can be populated through JSON.</p>
+      <p>The idea behind JSON population is pretty simple, properties reflect the name of the component and are followed by a progressive number. Here is a list of properties that are currently used/supported:</p>
+      <br />
+      <span class="code-word">button</span> contains an object reflecting props of
+      <a
+        href="https://ketchup.smeup.com/ketchup-showcase/#/button"
+      >kup-button</a>.
+      <br />
+      <span class="code-word">color</span> contains a CSS color string.
+      <br />
+      <span class="code-word">emphText</span> contains a string to be displayed as emphasized text.
+      <br />
+      <span class="code-word">image</span> contains an object reflecting props of
+      <a
+        href="https://ketchup.smeup.com/ketchup-showcase/#/image"
+      >kup-image</a>.
+      <br />
+      <span class="code-word">progressBar</span> contains an object reflecting props of
+      <a
+        href="https://ketchup.smeup.com/ketchup-showcase/#/progressbar"
+      >kup-progress-bar</a>.
+      <br />
+      <span class="code-word">text</span> contains a string to be displayed as generic text.
+      <br />
+      <br />
+      <p class="centered">Sample markup</p>
+      <code class="flat">{{ markupBasic }}</code>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardBasic',
+  data() {
+    return {
+      markupBasic: '<kup-card></kup-card>',
+    };
+  },
+};
+</script>
