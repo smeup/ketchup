d053342f7565a561a1fa2fecbcf55b1b63adff24
foscili-smeup <lucafoscili@icloud.com>
2020-06-15 15:48:08 +0200

    Grid showcase.

--- packages/ketchup-showcase/src/views/basic/grid/Grid.vue
+++ packages/ketchup-showcase/src/views/basic/grid/Grid.vue
@@ -4,16 +4,26 @@
       <template v-slot:0>
         <grid-demo></grid-demo>
       </template>
+      <template v-slot:1>
+        <grid-basic></grid-basic>
+      </template>
+      <template v-slot:2>
+        <grid-singleline></grid-singleline>
+      </template>
     </comp>
   </div>
 </template>
 
 <script>
 import GridDemo from '@/views/basic/grid/examples/GridDemo';
+import GridBasic from '@/views/basic/grid/examples/GridBasic';
+import GridSingleline from '@/views/basic/grid/examples/GridSingleline';
 import Comp from '@/views/advanced/page/templates/Comp';
 export default {
   components: {
     GridDemo,
+    GridBasic,
+    GridSingleline,
     Comp,
   },
   data() {
@@ -21,7 +31,7 @@ export default {
       giturl:
         'https://github.com/smeup/ketchup/tree/develop/packages/ketchup/src/components/kup-grid',
       headtitle: 'Grid',
-      titles: ['Playground'],
+      titles: ['Playground', 'Basic usage', 'Single line'],
     };
   },
 };
