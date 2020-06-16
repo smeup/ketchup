321912677d54a84c3c1dea8ee2caceb702f31043
foscili-smeup <lucafoscili@icloud.com>
2020-06-16 12:17:15 +0200

    Changed how themes work.

---
+++ packages/ketchup-showcase/public/kup-themes.js
@@ -0,0 +1,52 @@
+const themes = JSON.parse(`{
+    "dark": {
+        "cssVariables": {
+            "--kup-background-color": "#2d2d2d",
+            "--kup-header-background-color": "#2d2d2d",
+            "--kup-drawer-background-color": "#1f1f1f",
+            "--kup-main-color": "#82f0e2",
+            "--kup-display-mode": "block",
+            "--kup-font-family": "Lato, sans-serif",
+            "--kup-font-size": "14px",
+            "--kup-text-on-main-color": "#555555",
+            "--kup-text-color": "#f5f5f5",
+            "--kup-icon-color": "#e0e0e0",
+            "--kup-hover-background-color": "#3c3c3c",
+            "--kup-hover-color": "#dddddd",
+            "--kup-border-color": "#535353",
+            "--kup-disabled-text-color": "#7e7e7e",
+            "--kup-disabled-background-color": "#3c3c3c",
+            "--kup-field-background-color": "#2a2a2a",
+            "--kup-chip-background-color": "#222222",
+            "--kup-spinner-color": "#f2e114",
+            "--kup-title-background-color": "#111111"
+        },
+        "customStyles": {
+            "kup-button": "#kup-component button { letter-spacing: 0px; text-transform: unset; }",
+            "kup-text-field": "#kup-component .mdc-text-field { background-color: transparent; }"
+        }
+    },
+    "default": {
+        "cssVariables": {
+            "--kup-background-color": "#ffffff",
+            "--kup-header-background-color": "#2e2e2e",
+            "--kup-drawer-background-color": "#ffffff",
+            "--kup-main-color": "#d64325",
+            "--kup-display-mode": "block",
+            "--kup-font-family": "Roboto, sans-serif",
+            "--kup-font-size": "14px",
+            "--kup-text-on-main-color": "#ffffff",
+            "--kup-text-color": "#2e2e2e",
+            "--kup-icon-color": "#505050",
+            "--kup-hover-background-color": "#f0f0f0",
+            "--kup-hover-color": "#545454",
+            "--kup-border-color": "#e0e0e0",
+            "--kup-disabled-text-color": "#5c5c5c",
+            "--kup-disabled-background-color": "#eaeaea",
+            "--kup-field-background-color": "#fafafa",
+            "--kup-chip-background-color": "#eaeaea",
+            "--kup-spinner-color": "#eaa710",
+            "--kup-title-background-color": "#f1f3f4"
+        }
+    }
+}`);
