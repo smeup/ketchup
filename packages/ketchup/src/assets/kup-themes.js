4f75d060871166df16f0516d46f596d45cf08a04
foscili-smeup <lucafoscili@icloud.com>
2020-06-16 17:00:52 +0200

    New themes.

--- packages/ketchup/src/assets/kup-themes.js
+++ packages/ketchup/src/assets/kup-themes.js
@@ -20,10 +20,6 @@ const themes = JSON.parse(`{
             "--kup-chip-background-color": "#222222",
             "--kup-spinner-color": "#f2e114",
             "--kup-title-background-color": "#111111"
-        },
-        "customStyles": {
-            "kup-button": "#kup-component button { letter-spacing: 0px; text-transform: unset; }",
-            "kup-text-field": "#kup-component .mdc-text-field { background-color: transparent; }"
         }
     },
     "default": {
@@ -44,6 +40,75 @@ const themes = JSON.parse(`{
             "--kup-disabled-text-color": "#5c5c5c",
             "--kup-disabled-background-color": "#eaeaea",
             "--kup-field-background-color": "#fafafa",
+            "--kup-chip-background-color": "#eaeaea",
+            "--kup-spinner-color": "#eaa710",
+            "--kup-title-background-color": "#f1f3f4"
+        }
+    },
+    "graphite": {
+        "cssVariables": {
+            "--kup-background-color": "#ffffff",
+            "--kup-header-background-color": "#535353",
+            "--kup-drawer-background-color": "#ffffff",
+            "--kup-main-color": "#888888",
+            "--kup-display-mode": "block",
+            "--kup-font-family": "Roboto, sans-serif",
+            "--kup-font-size": "13px",
+            "--kup-text-on-main-color": "#ffffff",
+            "--kup-text-color": "#545454",
+            "--kup-icon-color": "#808080",
+            "--kup-hover-background-color": "#f0f0f0",
+            "--kup-hover-color": "#545454",
+            "--kup-border-color": "#e0e0e0",
+            "--kup-disabled-text-color": "#5c5c5c",
+            "--kup-disabled-background-color": "#eaeaea",
+            "--kup-field-background-color": "#fafafa",
+            "--kup-chip-background-color": "#eaeaea",
+            "--kup-spinner-color": "#eaa710",
+            "--kup-title-background-color": "#f0f0f0"
+        }
+    },
+    "ocean": {
+        "cssVariables": {
+            "--kup-background-color": "#e9f1ff",
+            "--kup-header-background-color": "#001d3e",
+            "--kup-drawer-background-color": "#e6f1ff",
+            "--kup-main-color": "#0081c5",
+            "--kup-display-mode": "block",
+            "--kup-font-family": "Lato, sans-serif",
+            "--kup-font-size": "16px",
+            "--kup-text-on-main-color": "#ffffff",
+            "--kup-text-color": "#1b1b1b",
+            "--kup-icon-color": "#505050",
+            "--kup-hover-background-color": "#cfe8ff",
+            "--kup-hover-color": "#545454",
+            "--kup-border-color": "#e0e0e0",
+            "--kup-disabled-text-color": "#5c5c5c",
+            "--kup-disabled-background-color": "#eaeaea",
+            "--kup-field-background-color": "#dce9ff",
+            "--kup-chip-background-color": "#eaeaea",
+            "--kup-spinner-color": "#eaa710",
+            "--kup-title-background-color": "#f1f3f4"
+        }
+    },
+    "wildlife": {
+        "cssVariables": {
+            "--kup-background-color": "#f7fff6",
+            "--kup-header-background-color": "#095a1f",
+            "--kup-drawer-background-color": "#dbfbd5",
+            "--kup-main-color": "#0fa918",
+            "--kup-display-mode": "block",
+            "--kup-font-family": "Roboto, sans-serif",
+            "--kup-font-size": "16px",
+            "--kup-text-on-main-color": "#ffffff",
+            "--kup-text-color": "#000000",
+            "--kup-icon-color": "#333333",
+            "--kup-hover-background-color": "#63ab46",
+            "--kup-hover-color": "#ffffff",
+            "--kup-border-color": "#e0e0e0",
+            "--kup-disabled-text-color": "#5c5c5c",
+            "--kup-disabled-background-color": "#eaeaea",
+            "--kup-field-background-color": "transparent",
             "--kup-chip-background-color": "#eaeaea",
             "--kup-spinner-color": "#eaa710",
             "--kup-title-background-color": "#f1f3f4"
