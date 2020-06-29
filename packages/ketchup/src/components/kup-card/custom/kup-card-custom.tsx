fe1d7ff53b7e2f48b61c0c49f914ce4bacb738f0
foscili-smeup <lucafoscili@icloud.com>
2020-06-29 11:44:27 +0200

    Layout 6.

--- packages/ketchup/src/components/kup-card/custom/kup-card-custom.tsx
+++ packages/ketchup/src/components/kup-card/custom/kup-card-custom.tsx
@@ -196,6 +196,48 @@ export function create5(layout: number, data: ComponentCardElement) {
     );
 }
 
+export function create6(layout: number, data: ComponentCardElement) {
+    let componentClass = 'custom-layout-' + layout;
+    let CSSVariables = {
+        ['--color-1']: data['color1'],
+    };
+    let chip1: KupChip = <kup-chip id="chips1" {...data['chip1']}></kup-chip>;
+    let image1: KupImage = (
+        <kup-image
+            id="image1"
+            color={data['color1']}
+            sizeX="24px"
+            sizeY="24px"
+            {...data['image1']}
+        ></kup-image>
+    );
+    return (
+        <div id="expandable-panel" class={componentClass} style={CSSVariables}>
+            <div class="section-1">
+                <div class="sub-1">
+                    <div class="text">{data['text1']}</div>
+                </div>
+                <div class="sub-2">
+                    <div class="text">{data['text2']}</div>
+                </div>
+                <div class="sub-3">
+                    <div class="image">{image1}</div>
+                    <div class="text">{data['text3']}</div>
+                </div>
+                <div class="sub-4">{chip1}</div>
+            </div>
+            <div class="section-2">
+                <kup-button
+                    toggable
+                    iconOff="keyboard_arrow_down"
+                    onKupButtonClick={(e: any) => expandPanel(e)}
+                    icon="keyboard_arrow_up"
+                ></kup-button>
+            </div>
+        </div>
+    );
+}
+
 function expandPanel(e) {
     let el = e.target
         .closest('#kup-component')
