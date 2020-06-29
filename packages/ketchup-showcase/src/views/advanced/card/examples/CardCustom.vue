cd7ea2cc6a3d2ebd3605d6935326152c70d8cad1
foscili-smeup <lucafoscili@icloud.com>
2020-06-29 10:32:36 +0200

    New card layout showcase (custom 4).

--- packages/ketchup-showcase/src/views/advanced/card/examples/CardCustom.vue
+++ packages/ketchup-showcase/src/views/advanced/card/examples/CardCustom.vue
@@ -71,7 +71,7 @@
         <span class="code-word">Layout 3</span> settings:
         <br />
         <br />
-        <span class="code-word">- color1</span>: Sets the color of the top bar and of text4 and text6.
+        <span class="code-word">- color1</span>: Sets the color of the top bar..
         <br />
         <span class="code-word">- image1</span>: Avatar.
         <br />
@@ -91,6 +91,32 @@
         </div>
       </div>
     </div>
+    <div class="demo-wrapper">
+      <p>
+        <span class="code-word">Layout 4</span> settings:
+        <br />
+        <br />
+        <span class="code-word">- color1</span>: Sets the color of the side bar.
+        <br />
+        <span class="code-word">- chip1</span>: Chips displayed after clicking on the bottom bar.
+        <br />
+        <span class="code-word">- text1, text2, text3</span>: Generic text, check the mock-up below to see how they are positioned.
+        <br />
+        <br />This layout features an expandable view, by clicking on the arrow button the height of the card will be set to fit its content. The bottom bar containing this button is 50px tall, so keep this in mind when you set the size of the card.
+      </p>
+      <br />
+      <div class="demo-container">
+        <div class="kup-container">
+          <kup-card
+            size-x="300px"
+            size-y="300px"
+            layout-number="4"
+            layout-family="custom"
+            :data.prop="data"
+          ></kup-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -100,9 +126,68 @@ export default {
   data() {
     return {
       data: {
+        button1: { icon: 'widgets', flat: true, title: 'button1' },
+        chip1: {
+          title: 'chip1',
+          data: [
+            {
+              value: '1_chip',
+              icon: 'filter_1',
+              label: 'First chip',
+              checked: false,
+            },
+            {
+              value: '2_chip',
+              icon: 'filter_2',
+              label: 'Second chip',
+              checked: true,
+            },
+            {
+              value: '3_chip',
+              icon: 'filter_3',
+              label: 'Third chip',
+              checked: false,
+            },
+            {
+              value: '4_chip',
+              icon: 'filter_4',
+              label: 'Fourth chip',
+              checked: false,
+            },
+            {
+              value: '5_chip',
+              icon: 'filter_5',
+              label: 'Fifth chip',
+              checked: false,
+            },
+            {
+              value: '6_chip',
+              icon: 'filter_6',
+              label: 'Sixth chip',
+              checked: false,
+            },
+            {
+              value: '7_chip',
+              icon: 'filter_7',
+              label: 'Seventh chip',
+              checked: false,
+            },
+            {
+              value: '8_chip',
+              icon: 'filter_8',
+              label: 'Eighth chip',
+              checked: false,
+            },
+            {
+              value: '9_chip',
+              icon: 'filter_9',
+              label: 'Ninth chip',
+              checked: false,
+            },
+          ],
+        },
         color1: 'var(--kup-spinner-color)',
         color2: 'var(--kup-border-color)',
-        button1: { icon: 'widgets', flat: true, title: 'button1' },
         image1: {
           data: [
             {
