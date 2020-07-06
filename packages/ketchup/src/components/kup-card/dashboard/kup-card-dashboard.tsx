9bb35abb61b2f0026832648adef73a2dafb9aadc
foscili-smeup <lucafoscili@icloud.com>
2020-07-06 17:51:54 +0200

    Dashboard layout 2.

--- packages/ketchup/src/components/kup-card/dashboard/kup-card-dashboard.tsx
+++ packages/ketchup/src/components/kup-card/dashboard/kup-card-dashboard.tsx
@@ -20,3 +20,39 @@ export function create1(component: KupCard) {
         </div>
     );
 }
+
+export function create2(component: KupCard) {
+    let componentClass =
+        'dashboard-layout-' + component.layoutNumber + ' dashboard-card';
+    let CSSVariables = {
+        ['--color-1']: component.data['color1'],
+        ['--multiplier']: '1',
+    };
+    return (
+        <div class={componentClass} style={CSSVariables}>
+            <div class="dashboard-element">
+                <div class="icon">
+                    {component.data['image1'] && (
+                        <kup-image
+                            sizeX="1em"
+                            sizeY="1em"
+                            color={component.data['color1']}
+                            id="image1"
+                            customStyle="img {object-fit: contain;}"
+                            {...component.data['image1']}
+                        ></kup-image>
+                    )}
+                </div>
+                <div class="value-int">
+                    <div>{component.data['text1']},</div>
+                </div>
+                <div class="value-dec">
+                    <div>{component.data['text2']}</div>
+                </div>
+                <div class="unit">
+                    <div>{component.data['text3']}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
