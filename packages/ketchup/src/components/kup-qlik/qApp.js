9cf6a9b0e4a4d437d6aca4e5a19d61c737936082
cigno88 <cignoni.andrea@gmail.com>
2020-07-13 17:44:55 +0200

    qApp prefix bug fix

--- packages/ketchup/src/components/kup-qlik/qApp.js
+++ packages/ketchup/src/components/kup-qlik/qApp.js
@@ -1,8 +1,10 @@
 
 const qApp = async (config) =>{
   return new Promise((resolve) => {
+    console.log(config)
+    const prefix = (config.prefix !== '/') ? `/${config.prefix}/` : '/';
     window.require.config({
-      baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources",
+      baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + prefix + "resources",
     })
     window.require( ["js/qlik"], function ( qlik ) {
 
