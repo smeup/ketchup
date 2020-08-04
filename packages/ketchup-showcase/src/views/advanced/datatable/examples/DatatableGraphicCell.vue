02cb05314bc90fadb32c3a6423afe5be45f0cc75
foscili-smeup <lucafoscili@icloud.com>
2020-08-04 11:38:23 +0200

    Showcase: graphic cells lazy.

--- packages/ketchup-showcase/src/views/advanced/datatable/examples/DatatableGraphicCell.vue
+++ packages/ketchup-showcase/src/views/advanced/datatable/examples/DatatableGraphicCell.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h3>Filters enabled</h3>
-    <kup-data-table :data.prop="gcData"></kup-data-table>
+    <h3>Graphic cells</h3>
+    <kup-lazy component-name="kup-data-table" :data.prop="gcData"></kup-lazy>
   </div>
 </template>
 
@@ -10,287 +10,289 @@ export default {
   data() {
     return {
       gcData: {
-        columns: [
-          {
-            name: 'FLD1',
-            title: 'Value',
-          },
-          {
-            name: 'FLD2',
-            title: 'Graphic cell',
-            size: '20ch',
-          },
-        ],
-        rows: [
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
+        data: {
+          columns: [
+            {
+              name: 'FLD1',
+              title: 'Value',
+            },
+            {
+              name: 'FLD2',
+              title: 'Graphic cell',
+              size: '20ch',
+            },
+          ],
+          rows: [
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value:
+                    'This is drawn using CSS. The examples below use canvas.',
+                  options: true,
                 },
-                value:
-                  'This is drawn using CSS. The examples below use canvas.',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k: 'R255G128B000;20,58\\\\HEIGHT;60',
-                },
-                config: {
-                  data: [
-                    {
-                      shape: 'bar',
-                      color: 'rgb(255,128,0)',
-                      width: '20%',
-                      height: '60%',
-                    },
-                  ],
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k: 'R255G128B000;20,58\\\\HEIGHT;60',
+                  },
+                  config: {
+                    data: [
+                      {
+                        shape: 'bar',
+                        color: 'rgb(255,128,0)',
+                        width: '20%',
+                        height: '60%',
+                      },
+                    ],
+                  },
                 },
               },
             },
-          },
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value: 'R255G128B000;20,58\\\\HEIGHT;60',
+                  options: true,
                 },
-                value: 'R255G128B000;20,58\\\\HEIGHT;60',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k: 'R255G128B000;20,58\\\\HEIGHT;60',
-                },
-                config: {
-                  resource: 'R255G128B000;20,58\\\\HEIGHT;60',
-                  isCanvas: true,
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k: 'R255G128B000;20,58\\\\HEIGHT;60',
+                  },
+                  config: {
+                    resource: 'R255G128B000;20,58\\\\HEIGHT;60',
+                    isCanvas: true,
+                  },
                 },
               },
             },
-          },
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
-                },
-                value:
-                  'R255G000B000;12,4\\\\SEP;25,00\\\\R255G255B000;74,00\\\\GRID;20',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k:
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value:
                     'R255G000B000;12,4\\\\SEP;25,00\\\\R255G255B000;74,00\\\\GRID;20',
+                  options: true,
                 },
-                config: {
-                  resource:
-                    'R255G000B000;12,4\\\\SEP;25,00\\\\R255G255B000;74,00\\\\GRID;20',
-                  isCanvas: true,
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k:
+                      'R255G000B000;12,4\\\\SEP;25,00\\\\R255G255B000;74,00\\\\GRID;20',
+                  },
+                  config: {
+                    resource:
+                      'R255G000B000;12,4\\\\SEP;25,00\\\\R255G255B000;74,00\\\\GRID;20',
+                    isCanvas: true,
+                  },
                 },
               },
             },
-          },
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
-                },
-                value:
-                  'R000G255B128;33,3\\\\ARW;50,00\\\\R255G255B051;66,5\\\\R220G000B000;100,0\\\\GRID;3',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k:
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value:
                     'R000G255B128;33,3\\\\ARW;50,00\\\\R255G255B051;66,5\\\\R220G000B000;100,0\\\\GRID;3',
+                  options: true,
                 },
-                config: {
-                  resource:
-                    'R000G255B128;33,3\\\\ARW;50,00\\\\R255G255B051;66,5\\\\R220G000B000;100,0\\\\GRID;3',
-                  isCanvas: true,
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k:
+                      'R000G255B128;33,3\\\\ARW;50,00\\\\R255G255B051;66,5\\\\R220G000B000;100,0\\\\GRID;3',
+                  },
+                  config: {
+                    resource:
+                      'R000G255B128;33,3\\\\ARW;50,00\\\\R255G255B051;66,5\\\\R220G000B000;100,0\\\\GRID;3',
+                    isCanvas: true,
+                  },
                 },
               },
             },
-          },
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
-                },
-                value:
-                  'SHAPE;CIRCLE\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k:
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value:
                     'SHAPE;CIRCLE\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
+                  options: true,
                 },
-                config: {
-                  resource:
-                    'SHAPE;CIRCLE\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
-                  isCanvas: true,
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k:
+                      'SHAPE;CIRCLE\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
+                  },
+                  config: {
+                    resource:
+                      'SHAPE;CIRCLE\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
+                    isCanvas: true,
+                  },
                 },
               },
             },
-          },
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
-                },
-                value:
-                  'SHAPE;TRIR\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k:
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value:
                     'SHAPE;TRIR\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
+                  options: true,
                 },
-                config: {
-                  resource:
-                    'SHAPE;TRIR\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
-                  isCanvas: true,
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k:
+                      'SHAPE;TRIR\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
+                  },
+                  config: {
+                    resource:
+                      'SHAPE;TRIR\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
+                    isCanvas: true,
+                  },
                 },
               },
             },
-          },
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
-                },
-                value:
-                  'SHAPE;TRIL\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k:
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value:
                     'SHAPE;TRIL\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
+                  options: true,
                 },
-                config: {
-                  resource:
-                    'SHAPE;TRIL\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
-                  isCanvas: true,
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k:
+                      'SHAPE;TRIL\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
+                  },
+                  config: {
+                    resource:
+                      'SHAPE;TRIL\\\\R000G255B128;33,3\\\\R255G255B051;66,5\\\\R220G000B000;100,0',
+                    isCanvas: true,
+                  },
                 },
               },
             },
-          },
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
-                },
-                value:
-                  'SHAPE;TRIR;50\\\\BCOLOR;R102G255B178\\\\*NONE;33,3\\\\R255G255B051;66,5',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k:
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value:
                     'SHAPE;TRIR;50\\\\BCOLOR;R102G255B178\\\\*NONE;33,3\\\\R255G255B051;66,5',
+                  options: true,
                 },
-                config: {
-                  resource:
-                    'SHAPE;TRIR;50\\\\BCOLOR;R102G255B178\\\\*NONE;33,3\\\\R255G255B051;66,5',
-                  isCanvas: true,
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k:
+                      'SHAPE;TRIR;50\\\\BCOLOR;R102G255B178\\\\*NONE;33,3\\\\R255G255B051;66,5',
+                  },
+                  config: {
+                    resource:
+                      'SHAPE;TRIR;50\\\\BCOLOR;R102G255B178\\\\*NONE;33,3\\\\R255G255B051;66,5',
+                    isCanvas: true,
+                  },
                 },
               },
             },
-          },
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
-                },
-                value:
-                  'SHAPE;TRIR\\\\R000G255B128;33,3\\\\AND\\\\SHAPE;TRIL\\\\*NONE;33,3\\\\R255G255B051;66,6\\\\AND\\\\SHAPE;CIRCLE\\\\R000G000B255;66,6',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k:
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value:
                     'SHAPE;TRIR\\\\R000G255B128;33,3\\\\AND\\\\SHAPE;TRIL\\\\*NONE;33,3\\\\R255G255B051;66,6\\\\AND\\\\SHAPE;CIRCLE\\\\R000G000B255;66,6',
+                  options: true,
                 },
-                config: {
-                  resource:
-                    'SHAPE;TRIR\\\\R000G255B128;33,3\\\\AND\\\\SHAPE;TRIL\\\\*NONE;33,3\\\\R255G255B051;66,6\\\\AND\\\\SHAPE;CIRCLE\\\\R000G000B255;66,6',
-                  isCanvas: true,
-                },
-              },
-            },
-          },
-          {
-            cells: {
-              FLD1: {
-                obj: {
-                  t: '',
-                  p: '',
-                  k: '',
-                },
-                value: 'SHAPE;CIRCLE\\\\R000G255B128;100',
-                options: true,
-              },
-              FLD2: {
-                obj: {
-                  t: 'J4',
-                  p: 'BAR',
-                  k: 'SHAPE;CIRCLE\\\\R000G255B128;100',
-                },
-                config: {
-                  resource: 'SHAPE;CIRCLE\\\\R000G255B128;100',
-                  isCanvas: true,
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k:
+                      'SHAPE;TRIR\\\\R000G255B128;33,3\\\\AND\\\\SHAPE;TRIL\\\\*NONE;33,3\\\\R255G255B051;66,6\\\\AND\\\\SHAPE;CIRCLE\\\\R000G000B255;66,6',
+                  },
+                  config: {
+                    resource:
+                      'SHAPE;TRIR\\\\R000G255B128;33,3\\\\AND\\\\SHAPE;TRIL\\\\*NONE;33,3\\\\R255G255B051;66,6\\\\AND\\\\SHAPE;CIRCLE\\\\R000G000B255;66,6',
+                    isCanvas: true,
+                  },
                 },
               },
             },
-          },
-        ],
+            {
+              cells: {
+                FLD1: {
+                  obj: {
+                    t: '',
+                    p: '',
+                    k: '',
+                  },
+                  value: 'SHAPE;CIRCLE\\\\R000G255B128;100',
+                  options: true,
+                },
+                FLD2: {
+                  obj: {
+                    t: 'J4',
+                    p: 'BAR',
+                    k: 'SHAPE;CIRCLE\\\\R000G255B128;100',
+                  },
+                  config: {
+                    resource: 'SHAPE;CIRCLE\\\\R000G255B128;100',
+                    isCanvas: true,
+                  },
+                },
+              },
+            },
+          ],
+        },
       },
     };
   },
