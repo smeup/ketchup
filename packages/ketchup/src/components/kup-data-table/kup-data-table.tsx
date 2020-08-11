1af02b66d39a769b4889fc0edf5531fda6a9c1bc
foscili-smeup <lucafoscili@icloud.com>
2020-08-11 15:55:31 +0200

    Fixed weird behavior when dragging column.

--- packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
+++ packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
@@ -2140,6 +2140,7 @@ export class KupDataTable {
                                 this.dragStarterAttribute,
                                 ''
                             );
+                            console.log(e.target);
                             this.theadRef.setAttribute(
                                 this.dragFlagAttribute,
                                 ''
