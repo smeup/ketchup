d5b175b32752fc1d61f53c111534e3480f81edeb
Stefano Macconi <stefano.macconi@smeup.com>
2020-09-24 11:34:48 +0200

    minor

--- packages/ketchup/src/components/kup-drawer/kup-drawer.tsx
+++ packages/ketchup/src/components/kup-drawer/kup-drawer.tsx
@@ -1,38 +1,38 @@
 import { Component, h, Prop, State, Method } from '@stencil/core';
 
 @Component({
-	tag: 'kup-drawer',
-	styleUrl: 'kup-drawer.css',
-	shadow: true
+    tag: 'kup-drawer',
+    styleUrl: 'kup-drawer.css',
+    shadow: true,
 })
 export class KupDrawer {
-	@State() showContactInfo = false;
-	@Prop() menu: string;
-	@Prop({ reflect: true, mutable: true })
-	opened: boolean;
+    @State() showContactInfo = false;
+    @Prop() menu: string;
+    @Prop({ reflect: true, mutable: true })
+    opened: boolean;
 
-	onCloseDrawer() {
-		this.opened = false;
-	}
+    onCloseDrawer() {
+        this.opened = false;
+    }
 
-	@Method()
-	async open() {
-		this.opened = true;
-	}
+    @Method()
+    async open() {
+        this.opened = true;
+    }
 
-	render() {
-		let mainContent = <slot />;
+    render() {
+        let mainContent = <slot />;
 
-		return [
-			<div class="backdrop" onClick={() => this.onCloseDrawer()} />,
-			<aside>
-				<header>
-					<h1>{this.menu}</h1>
-					<button onClick={() => this.onCloseDrawer()}>X</button>
-				</header>
+        return [
+            <div class="backdrop" onClick={() => this.onCloseDrawer()} />,
+            <aside>
+                <header>
+                    <h1>{this.menu}</h1>
+                    <button onClick={() => this.onCloseDrawer()}>X</button>
+                </header>
 
-				<main>{mainContent}</main>
-			</aside>
-		];
-	}
+                <main>{mainContent}</main>
+            </aside>,
+        ];
+    }
 }
