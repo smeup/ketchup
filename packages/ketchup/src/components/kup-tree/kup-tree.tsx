e419de3abf8e798d8587b7b27995f88d4d9f6996
foscili-smeup <lucafoscili@icloud.com>
2020-11-09 13:58:45 +0100

    Expand/collapse children nodes on CTRL + Click.

--- packages/ketchup/src/components/kup-tree/kup-tree.tsx
+++ packages/ketchup/src/components/kup-tree/kup-tree.tsx
@@ -556,7 +556,11 @@ export class KupTree {
     }
 
     // When a TreeNode must be expanded or closed.
-    hdlTreeNodeExpanderClicked(treeNodeData: TreeNode, treeNodePath: string) {
+    hdlTreeNodeExpanderClicked(
+        treeNodeData: TreeNode,
+        treeNodePath: string,
+        ctrlKey: boolean
+    ) {
         // If the node is expandable
         if (treeNodeData.expandable) {
             // Always composes the tree node path as an array
@@ -570,6 +574,12 @@ export class KupTree {
                 treeNodeData[treeExpandedPropName] = !treeNodeData[
                     treeExpandedPropName
                 ];
+                if (ctrlKey) {
+                    this.handleChildren(
+                        treeNodeData,
+                        treeNodeData[treeExpandedPropName]
+                    );
+                }
                 this.forceUpdate();
                 if (treeNodeData[treeExpandedPropName]) {
                     // TreeNode is now expanded -> Fires expanded event
@@ -635,6 +645,18 @@ export class KupTree {
                     treeNodeData[treeExpandedPropName] = !treeNodeData[
                         treeExpandedPropName
                     ];
+                }
+            }
+        }
+    }
+
+    private handleChildren(TreeNode: TreeNode, expand: boolean) {
+        for (let index = 0; index < TreeNode.children.length; index++) {
+            let node = TreeNode.children[index];
+            if (!node.disabled) {
+                node[treeExpandedPropName] = expand;
+                if (node.children) {
+                    this.handleChildren(node, expand);
                 }
             }
         }
@@ -976,14 +998,16 @@ export class KupTree {
         }
         let treeExpandIcon = (
             <span
+                title="Expand/collapse children (CTRL + Click) "
                 class={expandClass}
                 onClick={
                     !treeNodeData.disabled
-                        ? (event) => {
+                        ? (event: MouseEvent) => {
                               event.stopPropagation();
                               this.hdlTreeNodeExpanderClicked(
                                   treeNodeData,
-                                  treeNodePath
+                                  treeNodePath,
+                                  event.ctrlKey
                               );
                           }
                         : null
