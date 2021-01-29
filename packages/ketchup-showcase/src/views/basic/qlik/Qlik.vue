17a5aaf55755b79b267b1d7ec4d3437b631a5b3c
Aziz Ur rehman <34032963+UhakeR@users.noreply.github.com>
2021-01-29 18:21:50 +0100

    created kup-qlik

---
+++ packages/ketchup-showcase/src/views/basic/qlik/Qlik.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <comp :giturl="giturl" :headtitle="headtitle" :titles="titles">
+      <template v-slot:0>
+        <qlik-basic></qlik-basic>
+      </template>
+      <template v-slot:1>
+        <qlik-how-to-use></qlik-how-to-use>
+      </template>
+      <template v-slot:2>
+        <qlik-class-name></qlik-class-name>
+      </template>
+      <template v-slot:3>
+        <qlik-event></qlik-event>
+      </template>
+      <template v-slot:4>
+        <qlik-toggle></qlik-toggle>
+      </template>
+    </comp>
+  </div>
+</template>
+
+<script>
+import QlikBasic from '@/views/basic/qlik/examples/QlikBasic';
+import QlikHowToUse from '@/views/basic/qlik/examples/QulikHowToUse';
+import QlikEvent from '@/views/basic/qlik/examples/QlikEvent';
+import Comp from '@/views/advanced/page/templates/Comp';
+export default {
+  components: {
+    QlikBasic,
+    QlikHowToUse,
+    QlikEvent,
+    Comp,
+  },
+  data() {
+    return {
+      giturl:
+        'https://github.com/smeup/ketchup/tree/develop/packages/ketchup/src/components/kup-qlik',
+      headtitle: 'Qlik',
+      titles: [
+        'Basic usage',
+        'How To Use',
+        'Qlik Event',
+      ],
+    };
+  },
+};
+</script>
---
+++ packages/ketchup-showcase/src/views/basic/qlik/examples/QlikEvent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="demo-wrapper">
+      <p> The drawer component has 2 events: </p>
+      <img src="{{schermata}}">
+      <img src="{{graficoLarge}}">
+      <img src="{{griglia}}">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DrawerBasic',
+  data() {
+    return {
+      schermata:
+        "./public./images./kup-qlik_esempio_schermata.PNG",
+      graficoLarge:
+        "./public./images./kup-qlik_esempio_grafico_large.PNG",
+      griglia:
+        "./public./images./kup-qlik_esempio_griglia.PNG",
+    };
+  },
+};
+</script>
