b9dc6a1251e5b235a7b82ed3ec8af216f05a7683
foscili-smeup <lucafoscili@icloud.com>
2021-03-17 13:00:07 +0100

    Improved typing, moved CSS to inline styles in order to let it be accessible from outside KupComponents.

--- packages/ketchup/src/utils/dynamic-position/dynamic-position.ts
+++ packages/ketchup/src/utils/dynamic-position/dynamic-position.ts
@@ -1,29 +1,33 @@
-// Element repositioning function - it starts when "el" class list gets updated and the class "dynamic-position-active" is found
-//
-// Arguments:
-//
-// - el       = element to reposition
-// - anchorEl = "el" position will be anchored to this element
-// - margin   = "el" distance from its parent in pixels
-// - above    = when true "el" will be always placed above its wrapper
-// - right    = when true "el" will be always placed on the right of its wrapper
-
 import type { KupDom } from '../kup-manager/kup-manager-declarations';
-import { DynamicallyPositionedElement } from './dynamic-position-declarations';
+import type { DynamicallyPositionedElement } from './dynamic-position-declarations';
 
 const dom: KupDom = document.documentElement as KupDom;
 
+/**
+ * This class is used to dynamically reposition HTML elements.
+ * @module DynamicPosition
+ */
 export class DynamicPosition {
     managedElements: Array<DynamicallyPositionedElement> = [];
-
+    /**
+     * Initializes the dynamic position.
+     * *
+     * @param {DynamicallyPositionedElement} el - Element to reposition.
+     * @param {HTMLElement} anchorEl - "el" position will be anchored to this element.
+     * @param {number} margin - "el" distance from its parent in pixels.
+     * @param {boolean} above - When true "el" will be always placed above its wrapper.
+     * @param {boolean} right - When true "el" will be always placed on the right of its wrapper.
+     */
     setup(
         el: DynamicallyPositionedElement,
         anchorEl: HTMLElement,
         margin?: number,
         above?: boolean,
         right?: boolean
-    ) {
+    ): void {
         el.classList.add('dynamic-position');
+        el.style.position = 'fixed';
+        el.style.zIndex = '1000';
         anchorEl.classList.add('dynamic-position-anchor');
         el.dynamicPosition = {
             anchor: anchorEl,
@@ -50,16 +54,28 @@ export class DynamicPosition {
         });
         this.managedElements.push(el);
     }
-
-    start(el: DynamicallyPositionedElement) {
+    /**
+     * Starts the process of dynamically reposition the element (which must be firstly initialized through this.setup()).
+     * *
+     * @param {DynamicallyPositionedElement} el - Element to reposition.
+     */
+    start(el: DynamicallyPositionedElement): void {
         el.classList.add('dynamic-position-active');
     }
-
-    stop(el: DynamicallyPositionedElement) {
+    /**
+     * Ends the process of dynamically reposition the element.
+     * *
+     * @param {DynamicallyPositionedElement} el - Element to reposition.
+     */
+    stop(el: DynamicallyPositionedElement): void {
         el.classList.remove('dynamic-position-active');
     }
-
-    run(el: DynamicallyPositionedElement) {
+    /**
+     * This function calculates where to place the element in order to correctly display it attached to its anchor point.
+     * *
+     * @param {DynamicallyPositionedElement} el - Element to reposition.
+     */
+    run(el: DynamicallyPositionedElement): void {
         if (
             !el.isConnected ||
             !el.classList.contains('dynamic-position-active')
