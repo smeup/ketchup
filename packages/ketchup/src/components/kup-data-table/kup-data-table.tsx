abaf80493cc57b36baf3f5e5108c82bd20d64c23
OrioneDG <delbono.giovanni@gmail.com>
2021-04-19 19:08:23 +0200

    Extra columns Flag

--- packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
+++ packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
@@ -172,6 +172,7 @@ export class KupDataTable {
                 this.expandGroups = state.expandGroups;
                 this.groupLabelDisplay = state.groupLabelDisplay;
                 this.density = state.density;
+                this.enableExtraColumns = state.enableExtraColumns;
                 this.enableSortableColumns = state.enableSortableColumns;
                 this.forceOneLine = state.forceOneLine;
                 this.globalFilter = state.globalFilter;
@@ -228,6 +229,15 @@ export class KupDataTable {
                 this.state.density = this.density;
                 somethingChanged = true;
             }
+            if (
+                !deepEqual(
+                    this.state.enableExtraColumns,
+                    this.enableExtraColumns
+                )
+            ) {
+                this.state.enableExtraColumns = this.enableExtraColumns;
+                somethingChanged = true;
+            }            
             if (
                 !deepEqual(
                     this.state.enableSortableColumns,
@@ -374,6 +384,10 @@ export class KupDataTable {
      * Defines the label to show when the table is empty.
      */
     @Prop() emptyDataLabel: string = 'Empty data';
+    /**
+     * Enables the extracolumns add buttons.
+     */
+     @Prop() enableExtraColumns: boolean = true;
     /**
      * Enables the sorting of columns by dragging them into different columns.
      */
@@ -2985,15 +2999,9 @@ export class KupDataTable {
                             />
                         );
                     }
-                    columnMenuItems.push(
-                        <li role="menuitem" class="button-row">
-                            <kup-button
-                                icon="book"
-                                title={groupLabel}
-                                onKupButtonClick={() =>
-                                    this.switchColumnGroup(group, column.name)
-                                }
-                            />
+                    let extraCol = null;
+                    if (this.enableExtraColumns) {
+                        extraCol = (
                             <kup-button
                                 icon="table-column-plus-after"
                                 title="Add column"
@@ -3004,6 +3012,18 @@ export class KupDataTable {
                                     this.closeMenuAndTooltip();
                                 }}
                             />
+                        );   
+                    }
+                    columnMenuItems.push(
+                        <li role="menuitem" class="button-row">
+                            <kup-button
+                                icon="book"
+                                title={groupLabel}
+                                onKupButtonClick={() =>
+                                    this.switchColumnGroup(group, column.name)
+                                }
+                            />
+                            {extraCol}
                             {actionHideCol}
                             <kup-button
                                 icon="extension"
