e612e8c3164433c34338d53581f96f3d7661eab0
Manuel Lini <manuel.lini@smeup.com>
2021-06-17 14:58:37 +0200

    Push checkbox in local showcase and fix form-field

--- packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
+++ packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
@@ -117,7 +117,6 @@ import {
 } from '../../f-components/f-chip/f-chip-declarations';
 import { FButtonStyling } from '../../f-components/f-button/f-button-declarations';
 import { FCheckbox } from '../../f-components/f-checkbox/f-checkbox';
-import { FCheckboxMDC } from '../../f-components/f-checkbox/f-checkbox-mdc';
 import { FCheckboxProps } from '../../f-components/f-checkbox/f-checkbox-declarations';
 import {
     GenericFilter,
@@ -1590,7 +1589,7 @@ export class KupDataTable {
                             checkboxes[index]['data-row']
                         );
                 }
-                FCheckboxMDC(checkboxes[index]);
+
             }
             //Row textfields
             const textfields: NodeListOf<HTMLElement> = root.querySelectorAll(
@@ -1637,7 +1636,7 @@ export class KupDataTable {
                             multiselectionCheckboxes[index]['data-row']
                         );
                 }
-                FCheckboxMDC(multiselectionCheckboxes[index] as HTMLElement);
+
             }
             //Row actions: expander
             const expanderRowActions: NodeListOf<HTMLElement> =
