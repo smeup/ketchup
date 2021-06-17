937294b21628e6d02350b8386aa8c6d95778b4f4
foscili-smeup <lucafoscili@icloud.com>
2021-06-17 09:34:41 +0200

    Fixed disabled switch.

--- packages/ketchup/src/f-components/f-switch/f-switch.tsx
+++ packages/ketchup/src/f-components/f-switch/f-switch.tsx
@@ -9,40 +9,46 @@ export const FSwitch: FunctionalComponent<FSwitchProps> = (
     props: FSwitchProps
 ) => {
     const classObj: Record<string, boolean> = {
-        'switch': true,
+        switch: true,
         'switch--checked': props.checked,
         'switch--disabled': props.disabled,
     };
 
     return (
         <div
-        class={`f-switch--wrapper ${
-            props.wrapperClass ? props.wrapperClass : ''
-        }`}
-        {...props.dataSet}
-        id={props.id}
-        title={props.title}
-    >
-        <div
-            class={`form-field ${props.leadingLabel ? 'form-field--align-end' : ''}`}>
-            <div class={`switch ${props.checked ? 'switch--checked' : ''}`}>
-                <div class="switch__track"></div>
-                <div class="switch__thumb-underlay">
-                    <div class="switch__thumb">
-                        <input
-                            type="checkbox"
-                            class="switch__native-control"
-                            role="switch"
-                            checked={props.checked}
-                            disabled={props.disabled}
-                            value={props.checked ? 'on' : 'off'}
-                        ></input>
-                         
+            class={`f-switch--wrapper ${
+                props.wrapperClass ? props.wrapperClass : ''
+            }`}
+            {...props.dataSet}
+            id={props.id}
+            title={props.title}
+        >
+            <div
+                class={`form-field ${
+                    props.leadingLabel ? 'form-field--align-end' : ''
+                }`}
+            >
+                <div
+                    class={`switch ${props.checked ? 'switch--checked' : ''} ${
+                        props.disabled ? 'switch--disabled' : ''
+                    }`}
+                >
+                    <div class="switch__track"></div>
+                    <div class="switch__thumb-underlay">
+                        <div class="switch__thumb">
+                            <input
+                                type="checkbox"
+                                class="switch__native-control"
+                                role="switch"
+                                checked={props.checked}
+                                disabled={props.disabled}
+                                value={props.checked ? 'on' : 'off'}
+                            ></input>
+                        </div>
                     </div>
                 </div>
+                <label>{props.label}</label>
             </div>
-            <label>{props.label}</label>
         </div>
-    </div>
     );
 };
