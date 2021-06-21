54b125251efac77e6d2cbfc8c83d95fd4e1faa6c
foscili-smeup <lucafoscili@icloud.com>
2021-06-21 16:58:19 +0200

    Row detail: moved card inside kup-data-table (was attached to body). Made editable by default.

--- packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
+++ packages/ketchup/src/components/kup-data-table/kup-data-table.tsx
@@ -1881,6 +1881,7 @@ export class KupDataTable {
                         '"]{background-color: rgba(var(--kup-text-color-rgb), 0.15); width: 10px;}',
                     data: transposedData,
                     density: 'medium',
+                    editableData: true,
                     headerIsPersistent: false,
                     id: this.rootElement.id ? this.rootElement.id : '',
                     rowsPerPage: 1000,
@@ -2020,7 +2021,9 @@ export class KupDataTable {
         this.detailCard.data = cardData;
         this.detailCard.style.left = x + 'px';
         this.detailCard.style.top = y + 'px';
-        document.body.append(this.detailCard);
+        this.rootElement
+            .shadowRoot
+            .append(this.detailCard);
     }
 
     private getEventDetails(el: HTMLElement): EventHandlerDetails {
