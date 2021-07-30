55ad56e129d8420d8b92017482504e7623629301
foscili-smeup <lucafoscili@icloud.com>
2021-07-30 17:31:59 +0200

    Restored debug.js in dev env.

--- packages/ketchup/src/assets/debug.js
+++ packages/ketchup/src/assets/debug.js
@@ -1,51 +1,17 @@
 // Replace the object below with yours.
-const compName = 'kup-list';
+const compName = 'kup-button';
 
 // Replace the props below with yours.
 const props = {
+    checked: true,
     customStyle: '',
-    data: [
-        {
-            text: 'Count',
-            value: 'Count',
-            selected: false,
-        },
-        {
-            text: 'Distinct',
-            value: 'Distinct',
-            selected: false,
-        },
-        {
-            text: 'Sum',
-            value: 'Sum',
-            selected: false,
-            separator: true,
-        },
-        {
-            text: 'Average',
-            value: 'Average',
-            selected: false,
-        },
-        {
-            text: 'Minimum',
-            value: 'Min',
-            selected: false,
-        },
-        {
-            text: 'Maximum',
-            value: 'Max',
-            selected: false,
-        },
-    ],
-    displayMode: 'description',
-    filter: '',
-    hideText: false,
-    isMenu: true,
-    menuVisible: true,
-    roleType: 'listbox',
-    selectable: true,
-    showIcons: false,
-    twoLine: false,
+    disabled: false,
+    icon: 'favorite',
+    iconOff: null,
+    label: null,
+    styling: 'raised',
+    toggable: false,
+    trailingIcon: false,
 };
 
 const wrapper = document.querySelector('#debug-wrapper');
