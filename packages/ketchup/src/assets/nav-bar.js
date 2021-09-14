1eccca0888f0912d3cdc7b2d80f8e90481735c60
foscili-smeup <45429703+foscili-smeup@users.noreply.github.com>
2021-09-14 08:45:00 +0200

    kup-nav-bar: improved dev page.

--- packages/ketchup/src/assets/nav-bar.js
+++ packages/ketchup/src/assets/nav-bar.js
@@ -1,42 +1,13 @@
-const drawer = document.querySelector('kup-drawer');
-const switches = document.querySelectorAll('kup-switch');
-const bars = document.querySelectorAll('kup-nav-bar');
-switches.forEach((el) => {
-    el.addEventListener('kup-switch-change', (e) => {
-        if (e.detail.value === 'on') {
-            document.documentElement.ketchup.theme.set('dark');
-        } else {
-            document.documentElement.ketchup.theme.set('ketchup');
-        }
-    });
-});
-bars.forEach((el) => {
-    el.image = {
-        resource:
-            'https://ketchup.smeup.com/ketchup-showcase/ketchup_logo_header.svg',
-    };
-    el.addEventListener('kup-navbar-menuclick', (e) => {
-        drawer.toggle().then(() => {
-            if (drawer.opened) {
-                e.detail.comp.rootElement.classList.add('padded');
-            } else {
-                e.detail.comp.rootElement.classList.remove('padded');
-            }
-        });
-    });
-});
-document.addEventListener('kup-dropdownbutton-itemclick', (e) => {
-    for (let index = 0; index < bars.length; index++) {
-        const el = bars[index];
-        if (e.detail.value !== el.id) {
-            el.classList.remove('visible');
-        } else {
-            el.classList.add('visible');
-        }
-    }
-});
-const stylingSelector = document.getElementById('styling-selector');
-stylingSelector.data = {
+const barEl = document.querySelector('kup-nav-bar');
+const drawerEl = document.querySelector('kup-drawer');
+const dropdownEl = document.querySelector('kup-dropdown-button');
+const switchEl = document.querySelector('kup-switch');
+
+barEl.image = {
+    resource:
+        'https://ketchup.smeup.com/ketchup-showcase/ketchup_logo_header.svg',
+};
+dropdownEl.data = {
     'kup-list': {
         data: [
             {
@@ -58,3 +29,23 @@ stylingSelector.data = {
         ],
     },
 };
+
+barEl.addEventListener('kup-navbar-menuclick', (e) => {
+    drawerEl.toggle().then(() => {
+        if (drawerEl.opened) {
+            e.detail.comp.rootElement.classList.add('padded');
+        } else {
+            e.detail.comp.rootElement.classList.remove('padded');
+        }
+    });
+});
+dropdownEl.addEventListener('kup-dropdownbutton-itemclick', (e) => {
+    barEl.styling = e.detail.value;
+});
+switchEl.addEventListener('kup-switch-change', (e) => {
+    if (e.detail.value === 'on') {
+        document.documentElement.ketchup.theme.set('dark');
+    } else {
+        document.documentElement.ketchup.theme.set('ketchup');
+    }
+});
