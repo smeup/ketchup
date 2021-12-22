5c29c8fd9e014107ff1dc7d7b5c74f82eabd4a36
Carina <carinagentiana.pasere@smeup.com>
2021-12-22 15:11:45 +0100

    up version 4.2.0-rc

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -67,7 +67,7 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup">v4.2.0-SNAPSHOT</a></div
+          ><a href="https://github.com/smeup/ketchup">v4.2.0-rc</a></div
         ></div
       >
     </kup-drawer>
