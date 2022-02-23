77130f8975592c957e6ac617374aecd5d3fc2b8a
foscili-smeup <45429703+foscili-smeup@users.noreply.github.com>
2022-02-23 14:54:11 +0100

    Showcase: added events page to KupMath.

--- packages/ketchup-showcase/src/views/framework/kup-math/KupMath.vue
+++ packages/ketchup-showcase/src/views/framework/kup-math/KupMath.vue
@@ -7,17 +7,22 @@
       <template v-slot:1>
         <kup-math-features></kup-math-features>
       </template>
+      <template v-slot:2>
+        <kup-math-events></kup-math-events>
+      </template>
     </comp>
   </div>
 </template>
 
 <script>
 import KupMathBasic from '@/views/framework/kup-math/examples/KupMathBasic';
+import KupMathEvents from '@/views/framework/kup-math/examples/KupMathEvents';
 import KupMathFeatures from '@/views/framework/kup-math/examples/KupMathFeatures';
 import Comp from '@/views/templates/Comp';
 export default {
   components: {
     KupMathBasic,
+    KupMathEvents,
     KupMathFeatures,
     Comp,
   },
@@ -26,7 +31,7 @@ export default {
       giturl:
         'https://github.com/smeup/ketchup/tree/develop/packages/ketchup/src/managers/kup-math',
       headtitle: 'KupMath',
-      titles: ['What is it?', 'Features'],
+      titles: ['What is it?', 'Features', 'Events'],
     };
   },
   title: 'Ketchup | KupMath',
