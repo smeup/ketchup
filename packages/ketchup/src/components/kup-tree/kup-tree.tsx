0f3b1e7c00f5b243ca20b75ad1762d0ec1fc866b
foscili-smeup <45429703+lucafoscili@users.noreply.github.com>
2022-05-06 10:01:22 +0200

    Bugfix (kup-tree): fixed missing details in contextmenu event.

--- packages/ketchup/src/components/kup-tree/kup-tree.tsx
+++ packages/ketchup/src/components/kup-tree/kup-tree.tsx
@@ -913,14 +913,61 @@ export class KupTree {
         this.openTotalMenu(column);
     }
 
-    private getEventDetails(el: HTMLElement): KupTreeEventHandlerDetails {
-        const isHeader: boolean = !!el.closest('thead'),
-            isBody: boolean = !!el.closest('tbody'),
-            isFooter: boolean = !!el.closest('tfoot'),
-            td = el.closest('td'),
-            th = el.closest('th'),
-            tr = el.closest('tr'),
-            filterRemove: HTMLElement = el.closest('th .filter-remove');
+    private getEventDetails(path: HTMLElement[]): KupTreeEventHandlerDetails {
+        let isHeader: boolean,
+            isBody: boolean,
+            isFooter: boolean,
+            td: HTMLElement,
+            th: HTMLElement,
+            tr: HTMLElement,
+            filterRemove: HTMLSpanElement;
+        if (path) {
+            for (let i = path.length - 1; i >= 0; i--) {
+                let p = path[i];
+                if (!p.tagName) {
+                    continue;
+                }
+                switch (p.tagName.toUpperCase()) {
+                    case 'THEAD': {
+                        isHeader = true;
+                        break;
+                    }
+                    case 'TBODY': {
+                        isBody = true;
+                        break;
+                    }
+                    case 'TFOOT': {
+                        isFooter = true;
+                        break;
+                    }
+                    case 'TD': {
+                        td = p;
+                        break;
+                    }
+                    case 'TH': {
+                        th = p;
+                        break;
+                    }
+                    case 'TR': {
+                        tr = p;
+                        break;
+                    }
+                    default: {
+                        if (
+                            p.classList.contains(
+                                KupThemeIconValues.FILTER_REMOVE.replace(
+                                    '--',
+                                    ''
+                                )
+                            )
+                        ) {
+                            filterRemove = p;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
         let cell: KupDataCell = null,
             column: KupDataColumn = null,
             row: KupDataRow = null;
@@ -963,7 +1010,7 @@ export class KupTree {
 
     private contextMenuHandler(e: PointerEvent): KupTreeEventHandlerDetails {
         e.preventDefault();
-        const details = this.getEventDetails(e.target as HTMLElement);
+        const details = this.getEventDetails(this.getEventPath(e.target));
         if (details.area === 'header') {
             if (details.th && details.column) {
                 this.openColumnMenu(details.column.name);
@@ -977,6 +1024,23 @@ export class KupTree {
             }
         }
         return details;
+    }
+
+    private getEventPath(currentEl: unknown): HTMLElement[] {
+        const path: HTMLElement[] = [];
+
+        while (
+            currentEl &&
+            currentEl !== this.rootElement &&
+            currentEl !== document.body
+        ) {
+            path.push(currentEl as HTMLElement);
+            currentEl = (currentEl as HTMLElement).parentNode
+                ? (currentEl as HTMLElement).parentNode
+                : (currentEl as ShadowRoot).host;
+        }
+
+        return path;
     }
 
     // When a TreeNode can be selected
@@ -1125,24 +1189,6 @@ export class KupTree {
                 }
             }
         }
-    }
-
-    private getEventPath(e: MouseEvent): HTMLElement[] {
-        let path: HTMLElement[] = [];
-
-        let currentEl: unknown = e.target as HTMLElement;
-        while (
-            currentEl &&
-            currentEl !== this.rootElement &&
-            currentEl !== document.body
-        ) {
-            path.push(currentEl as HTMLElement);
-            currentEl = (currentEl as HTMLElement).parentNode
-                ? (currentEl as HTMLElement).parentNode
-                : (currentEl as ShadowRoot).host;
-        }
-
-        return path;
     }
 
     private hasTotals() {
@@ -1643,7 +1689,11 @@ export class KupTree {
                         setSizes: true,
                     };
                     treeNodeCells.push(
-                        <td class={`grid-cell`} data-column={column.name}>
+                        <td
+                            class={`grid-cell`}
+                            data-cell={cell}
+                            data-column={column.name}
+                        >
                             <FCell {...cellProps}></FCell>
                         </td>
                     );
@@ -1737,6 +1787,7 @@ export class KupTree {
                         !treeNodeData.disabled &&
                         treeNodePath === this.selectedNodeString,
                 }}
+                data-row={treeNodeData}
                 data-tree-path={treeNodePath}
                 {...treeNodeOptions}
             >
