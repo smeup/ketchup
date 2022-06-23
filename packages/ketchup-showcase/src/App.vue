429e9409688d9042081fd15911bbcfad0ab8d1e2
Carina <carinagentiana.pasere@smeup.com>
2022-06-23 14:04:46 +0200

    up version 5.1.2

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -55,8 +55,8 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup/releases/tag/v5.1.1"
-            >v5.1.1</a
+          ><a href="https://github.com/smeup/ketchup/releases/tag/v5.1.2"
+            >v5.1.2</a
           ></div
         ></div
       >
