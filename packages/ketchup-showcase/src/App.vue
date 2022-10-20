3a8cad37d16ad5f80a5c644643d5b0c5a5ee83e3
Carina <carinagentiana.pasere@smeup.com>
2022-10-20 15:26:03 +0200

    up version 6.6.0

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -55,7 +55,9 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup">v6.6.0-SNAPSHOT</a></div
+          ><a href="https://github.com/smeup/ketchup/releases/tag/v6.6.0"
+            >v6.6.0</a
+          ></div
         ></div
       >
     </kup-drawer>
