59017e0c7f63435bf0118b296d5fe075037313b7
foscili-smeup <45429703+lucafoscili@users.noreply.github.com>
2022-12-20 11:59:52 +0100

    kup-data-table: insertMode instead of showInsertButton.

--- packages/ketchup/src/components.d.ts
+++ packages/ketchup/src/components.d.ts
@@ -25,7 +25,7 @@ import { FChipType } from "./f-components/f-chip/f-chip-declarations";
 import { KupColorPickerEventPayload } from "./components/kup-color-picker/kup-color-picker-declarations";
 import { KupComboboxEventPayload, KupComboboxIconClickEventPayload } from "./components/kup-combobox/kup-combobox-declarations";
 import { KupDashboardEventPayload, KupDataDashboard } from "./components/kup-dashboard/kup-dashboard-declarations";
-import { GroupLabelDisplayMode, GroupObject, KupDatatableClickEventPayload, KupDatatableColumnMenuEventPayload, KupDatatableColumnMoveEventPayload, KupDatatableColumnRemoveEventPayload, KupDataTableDataset, KupDatatableDeleteRowEventPayload, KupDatatableInsertRowEventPayload, KupDatatableLoadMoreClickEventPayload, KupDataTableRow, KupDatatableRowActionClickEventPayload, KupDatatableRowSelectedEventPayload, LoadMoreMode, PaginatorPos, SelectionMode, ShowGrid, SortObject, TotalsMap } from "./components/kup-data-table/kup-data-table-declarations";
+import { GroupLabelDisplayMode, GroupObject, KupDatatableClickEventPayload, KupDatatableColumnMenuEventPayload, KupDatatableColumnMoveEventPayload, KupDatatableColumnRemoveEventPayload, KupDataTableDataset, KupDatatableDeleteRowEventPayload, KupDataTableInsertMode, KupDatatableInsertRowEventPayload, KupDatatableLoadMoreClickEventPayload, KupDataTableRow, KupDatatableRowActionClickEventPayload, KupDatatableRowSelectedEventPayload, LoadMoreMode, PaginatorPos, SelectionMode, ShowGrid, SortObject, TotalsMap } from "./components/kup-data-table/kup-data-table-declarations";
 import { GenericFilter, KupGlobalFilterMode } from "./utils/filters/filters-declarations";
 import { KupDatePickerEventPayload } from "./components/kup-date-picker/kup-date-picker-declarations";
 import { KupDropdownButtonEventPayload } from "./components/kup-dropdown-button/kup-dropdown-button-declarations";
@@ -1176,6 +1176,11 @@ export namespace Components {
          */
         "hideColumn": (column: KupDataColumn) => Promise<void>;
         /**
+          * Enables insert mode.
+          * @default '''
+         */
+        "insertMode": KupDataTableInsertMode;
+        /**
           * Adds a new row to the list data
           * @param row new row
          */
@@ -1314,10 +1319,6 @@ export namespace Components {
           * @namespace KupDataTable.showHeader
          */
         "showHeader": boolean;
-        /**
-          * Enables the insert row button.
-         */
-        "showInsertButton": boolean;
         /**
           * If set to true, displays the button to load more records.
          */
@@ -4493,6 +4494,11 @@ declare namespace LocalJSX {
          */
         "headerIsPersistent"?: boolean;
         /**
+          * Enables insert mode.
+          * @default '''
+         */
+        "insertMode"?: KupDataTableInsertMode;
+        /**
           * When set to true, clicked-on rows will have a visual feedback.
           * @default false
          */
@@ -4635,10 +4641,6 @@ declare namespace LocalJSX {
           * @namespace KupDataTable.showHeader
          */
         "showHeader"?: boolean;
-        /**
-          * Enables the insert row button.
-         */
-        "showInsertButton"?: boolean;
         /**
           * If set to true, displays the button to load more records.
          */
