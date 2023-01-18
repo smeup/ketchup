ac54d3a4eec63aaf2225413c2d4d3ec2500c0f31
foscili-smeup <45429703+lucafoscili@users.noreply.github.com>
2023-01-18 14:56:23 +0100

    v7.0.0

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -55,7 +55,9 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup">v6.9.0-SNAPSHOT</a></div
+          ><a href="https://github.com/smeup/ketchup/releases/tag/v7.0.0"
+            >v7.0.0</a
+          ></div
         ></div
       >
     </kup-drawer>
