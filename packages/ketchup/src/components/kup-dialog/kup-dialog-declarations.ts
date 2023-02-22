f54b983e9bf347059e278e976b00a567d9597cbd
foscili-smeup <45429703+lucafoscili@users.noreply.github.com>
2023-02-22 15:07:50 +0100

    kup-dialog: template.

---
+++ packages/ketchup/src/components/kup-dialog/kup-dialog-declarations.ts
@@ -0,0 +1,7 @@
+/**
+ * Props of the kup-dialog component.
+ * Used to export every prop in an object.
+ */
+export enum KupDialogProps {
+    customStyle = 'Custom style of the component.',
+}
--- packages/ketchup/stencil.config.ts
+++ packages/ketchup/stencil.config.ts
@@ -34,6 +34,7 @@ export const config: Config = {
                 { src: 'data-table-performance.html' },
                 { src: 'date-picker.html' },
                 { src: 'debug.html' },
+                { src: 'dialog.html' },
                 { src: 'drawer.html' },
                 { src: 'dropdown-button.html' },
                 { src: 'echart.html' },
