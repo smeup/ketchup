4b6d1a35a90e0ec0ef7773d1b31b3749d4f39046
foscili-smeup <45429703+lucafoscili@users.noreply.github.com>
2023-02-27 11:46:17 +0100

    kup-dialog: showcase.

---
+++ packages/ketchup-showcase/src/views/components/basic/dialog/Dialog.vue
@@ -0,0 +1,33 @@
+<template>
+  <comp :giturl="giturl" :headtitle="headtitle" :titles="titles">
+    <template v-slot:0>
+      <dialog-demo></dialog-demo>
+    </template>
+    <template v-slot:1>
+      <dialog-basic></dialog-basic>
+    </template>
+  </comp>
+</template>
+
+<script>
+import DialogBasic from '@/views/components/basic/dialog/examples/DialogBasic';
+import DialogDemo from '@/views/components/basic/dialog/examples/DialogDemo';
+import Comp from '@/views/templates/Comp';
+
+export default {
+  components: {
+    DialogBasic,
+    DialogDemo,
+    Comp,
+  },
+  data() {
+    return {
+      giturl:
+        'https://github.com/smeup/ketchup/tree/develop/packages/ketchup/src/components/kup-dialog',
+      headtitle: 'Dialog',
+      titles: ['Playground', 'Basic usage'],
+    };
+  },
+  title: 'Ketchup | Dialog',
+};
+</script>
