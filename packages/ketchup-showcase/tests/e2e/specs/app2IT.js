9477b2de2b0463dc55b9ebb7f14204582e8ca99d
foscili-smeup <45429703+lucafoscili@users.noreply.github.com>
2023-04-06 16:41:53 +0200

    Removed e2e dependencies and tests (also removed showcase testing altogether).

--- packages/ketchup-showcase/tests/e2e/specs/app2IT.js
+++
@@ -1,6 +0,0 @@
-describe('Showcase e2e - sample test 2', () => {
-  it.skip('Visits the app root url 2', () => {
-    cy.visit('/');
-    cy.get('h1').should('contain', 'Ketchup showcase');
-  });
-});
