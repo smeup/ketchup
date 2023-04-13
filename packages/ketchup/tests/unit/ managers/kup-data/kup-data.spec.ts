049c0554ca92b9945039ab2d3c5df388b1c3e43b
chiarazambe-smeup <chiara.zambelli@smeup.com>
2023-04-13 11:54:23 +0200

    jest improvements: added it for KupData

---
+++ packages/ketchup/tests/unit/ managers/kup-data/kup-data.spec.ts
@@ -0,0 +1,56 @@
+import { KupManager } from '../../../../src/managers/kup-manager/kup-manager';
+import { KupDom } from '../../../../src/managers/kup-manager/kup-manager-declarations';
+
+import sampleKupDataDataset from '../../../resources/mock/kup-data-dataset-luckynrs.json';
+import sampleNewColumns from '../../../resources/mock/kup-data-new-columns-luckynrs.json';
+import {
+    KupDataColumn,
+    KupDataDataset,
+    KupDataNewColumn,
+} from '../../../../src/managers/kup-data/kup-data-declarations';
+
+const dom: KupDom = document.documentElement as KupDom;
+if (!dom.ketchup) {
+    dom.ketchup = new KupManager();
+}
+
+const dataDataset: KupDataDataset = sampleKupDataDataset;
+const dataNewColumns: KupDataNewColumn[] = sampleNewColumns;
+
+describe('kup dataset manipulation', () => {
+    it('groups and counts with specific ranges and filters for a resulting column', () => {
+        const inpDataDataset = { ...dataDataset };
+
+        const resultingColumn: KupDataColumn = {
+            name: '2099',
+            title: 'Lucky numbers 20-99',
+        };
+
+        const outDataDataset = dom.ketchup.data.rangedDistinct(
+            inpDataDataset,
+            dataNewColumns,
+            resultingColumn
+        );
+
+        expect(outDataDataset.rows && outDataDataset.rows.length).toEqual(3);
+
+        expect(
+            outDataDataset.rows &&
+                outDataDataset.rows[0] &&
+                outDataDataset.rows[0].cells &&
+                outDataDataset.rows[0].cells['2099'].value
+        ).toEqual('5');
+        expect(
+            outDataDataset.rows &&
+                outDataDataset.rows[1] &&
+                outDataDataset.rows[1].cells &&
+                outDataDataset.rows[1].cells['2099'].value
+        ).toEqual('5');
+        expect(
+            outDataDataset.rows &&
+                outDataDataset.rows[2] &&
+                outDataDataset.rows[2].cells &&
+                outDataDataset.rows[2].cells['2099'].value
+        ).toEqual('2');
+    });
+});
