cee235b54bfd3ef8f4b3d332e140079b8f61e04d
foscili-smeup <45429703+lucafoscili@users.noreply.github.com>
2023-05-04 09:58:28 +0200

    Fix v.

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -55,8 +55,8 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup/releases/tag/v8.0.0"
-            >vv8.0.0</a
+          ><a href="https://github.com/smeup/ketchup/releases/tag/8.0.0"
+            >v8.0.0</a
           ></div
         ></div
       >
