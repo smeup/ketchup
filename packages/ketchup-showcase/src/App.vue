f807b8188d625a7790f4f2fb2857b7e7afa6b444
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-02-07 14:40:24 +0100

    9.6.0

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -55,8 +55,8 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup/releases/tag/v9.5.2"
-            >v9.5.2</a
+          ><a href="https://github.com/smeup/ketchup/releases/tag/v9.6.0"
+            >v9.6.0</a
           ></div
         ></div
       >
