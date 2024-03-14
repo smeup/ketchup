2aedbcf66d052ec983d7fdc8eed0d9d426243aa4
p-bemportato <pierluigi.bemportato@apuliasoft.com>
2024-03-14 16:23:46 +0000

    chore: KupInputPanelData declaration

--- packages/ketchup/src/components/kup-input-panel/kup-input-panel-declarations.ts
+++ packages/ketchup/src/components/kup-input-panel/kup-input-panel-declarations.ts
@@ -1,1 +1,59 @@
-export interface KupInputPanelData {}
+import { FCellShapes } from '../../f-components/f-cell/f-cell-declarations';
+import { KupObj } from '../../managers/kup-objects/kup-objects-declarations';
+
+export interface KupInputPanelData {
+    columns?: KupInputPanelColumn[];
+    rows?: KupInputPanelRow[];
+}
+
+export interface KupInputPanelColumn {
+    name: string;
+    title?: string;
+    visible?: boolean;
+}
+
+export interface KupInputPanelRow {
+    cells?: KupInputPanelRowCells;
+    layout?: KupInputPanelLayout;
+}
+
+export interface KupInputPanelRowCells {
+    [key: string]: KupInputPanelCell;
+}
+
+export interface KupInputPanelCell {
+    value?: string;
+    obj?: KupObj;
+    options?: string[];
+    icon?: string;
+    editable?: boolean;
+    mandatory?: boolean;
+    shape?: FCellShapes;
+    fun?: string;
+}
+
+export interface KupInputPanelLayout {
+    horizontal?: boolean;
+    sections?: KupInputPanelLayoutSection[];
+}
+
+export interface KupInputPanelLayoutSection {
+    id?: string;
+    content?: KupInputPanelLayoutField[];
+    sections?: KupInputPanelLayoutSection[];
+    dim?: string;
+    horizontal?: boolean;
+    gridCols?: number;
+    gridRows?: number;
+    gap?: number;
+}
+
+export interface KupInputPanelLayoutField {
+    id: string;
+    colSpan?: number;
+    colStart?: number;
+    colEnd?: number;
+    rowSpan?: number;
+    rowStart?: number;
+    rowEnd?: number;
+}
