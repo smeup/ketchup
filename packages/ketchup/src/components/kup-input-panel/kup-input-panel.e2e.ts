1c55d7003f482e977e687f343f49478ec735237c
Valerio Como <valerio.como@apuliasoft.com>
2024-03-26 12:18:51 +0000

    chore: second e2e test

--- packages/ketchup/src/components/kup-input-panel/kup-input-panel.e2e.ts
+++ packages/ketchup/src/components/kup-input-panel/kup-input-panel.e2e.ts
@@ -1,5 +1,16 @@
+import { newE2EPage } from '@stencil/core/testing';
+
 describe('kup-input-panel', () => {
     it('it runs', () => {
         expect(true).toBeTruthy();
+    });
+
+    //TODO: riparti da qui
+    it('renders', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent('<kup-input-panel></kup-input-panel>');
+        const element = await page.find('kup-input-panel');
+        expect(element).toHaveClass('hydrated');
     });
 });
