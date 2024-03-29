f905506a2c582765be7db418bac0ffa67ee3f6c3
p-bemportato <pierluigi.bemportato@apuliasoft.com>
2024-03-29 08:55:17 +0000

    feat: autocomplete e2e test

--- packages/ketchup/src/components/kup-input-panel/kup-input-panel.e2e.ts
+++ packages/ketchup/src/components/kup-input-panel/kup-input-panel.e2e.ts
@@ -59,7 +59,6 @@ describe('kup-input-panel', () => {
         );
         expect(textFields).toHaveLength(data.columns.length);
 
-        // TODO valutare questo for utilizzato per mantenere l'asincronicità
         for (const [i, textField] of textFields.entries()) {
             const label = await textField.find('label');
             expect(label).not.toBeNull();
@@ -81,5 +80,109 @@ describe('kup-input-panel', () => {
             const updatedValue = await input.getProperty('value');
             expect(updatedValue).toBe('string');
         }
+    });
+
+    it('renders autocomplete', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent(
+            '<kup-input-panel></kup-input-panel> <div kup-dynamic-position></div>'
+        );
+        const inputPanel = await page.find('kup-input-panel');
+        const data = {
+            columns: [
+                {
+                    name: 'CIT',
+                    title: 'City',
+                    visible: true,
+                },
+            ],
+            rows: [
+                {
+                    cells: {
+                        CIT: {
+                            value: '',
+                            obj: {
+                                t: '',
+                                p: '',
+                                k: '',
+                            },
+                            editable: true,
+                            mandatory: true,
+                            options: [
+                                'Florence',
+                                'Venice',
+                                'Rome',
+                                'Madrid',
+                                'Barcelona',
+                                'Seville',
+                                'Berlin',
+                                'Munich',
+                                'Hamburg',
+                                'Paris',
+                                'Marseille',
+                                'Lyon',
+                                'Lisbon',
+                                'Porto',
+                                'Faro',
+                                'London',
+                                'Manchester',
+                                'Liverpool',
+                            ],
+                            shape: 'ACP',
+                        },
+                    },
+                },
+            ],
+        };
+
+        inputPanel.setProperty('data', data);
+
+        await page.waitForChanges();
+
+        const inputPanelContent = await page.find(
+            'kup-input-panel >>> form.input-panel'
+        );
+        expect(inputPanelContent).not.toBeNull();
+
+        const autocompleteCell = await inputPanelContent.find(
+            '.f-cell.autocomplete-cell'
+        );
+        expect(autocompleteCell).not.toBeNull();
+
+        const autocompleteTextfield = await autocompleteCell.find(
+            'kup-autocomplete >>> div.f-text-field'
+        );
+        expect(autocompleteTextfield).not.toBeNull();
+
+        const label = await autocompleteTextfield.find('label');
+        expect(label).not.toBeNull();
+        expect(label).toEqualText(data.columns[0].title);
+
+        const input = await autocompleteTextfield.find('input');
+        expect(input).not.toBeNull();
+
+        await input.focus();
+        await input.press('KeyR');
+        await input.press('KeyO');
+        const updatedValue1 = await input.getProperty('value');
+        expect(updatedValue1).toBe('ro');
+
+        await page.waitForChanges();
+        await page.waitForChanges();
+
+        const list = await page.find('div[kup-dynamic-position] kup-list');
+        expect(list).not.toBeNull();
+
+        const listOptions = await page.findAll('kup-list >>> ul.list li');
+        expect(listOptions).not.toBeNull();
+        expect(listOptions).toHaveLength(2);
+
+        const firstOptionValue = await listOptions[0].find('span');
+        expect(firstOptionValue).toEqualText('Rome');
+        await firstOptionValue.click();
+
+        const updatedValue = await input.getProperty('value');
+        expect(updatedValue).toBe('Rome');
     });
 });
