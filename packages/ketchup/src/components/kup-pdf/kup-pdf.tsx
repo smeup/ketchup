22e6ec8c515f76e944ddb375d90f03d11a7e47dc
Hiren <Hiren@gmail.com>
2024-04-02 16:54:07 +0530

    kup-pdf: Fixed responsive and added watcher

--- packages/ketchup/src/components/kup-pdf/kup-pdf.tsx
+++ packages/ketchup/src/components/kup-pdf/kup-pdf.tsx
@@ -8,6 +8,7 @@ import {
     Host,
     Method,
     Prop,
+    Watch,
 } from '@stencil/core';
 import {
     KupManager,
@@ -62,6 +63,16 @@ export class KupPdf {
         bubbles: true,
     })
     kupReady: EventEmitter<KupEventPayload>;
+
+    /*-------------------------------------------------*/
+    /*                  W a t c h e r s                */
+    /*-------------------------------------------------*/
+
+    @Watch('pdfPath')
+    onPdfPathChange() {
+        this.wrapperRef.innerHTML = '';
+        this.renderPdf();
+    }
 
     /*-------------------------------------------------*/
     /*           P u b l i c   M e t h o d s           */
@@ -121,44 +132,38 @@ export class KupPdf {
     }
 
     async renderPdf() {
-        const loadingPdfTask = pdfjsLib.getDocument({
-            url: this.pdfPath,
-            withCredentials: true,
-        });
-        const pdf = await loadingPdfTask.promise;
-        const pdfContainer = this.wrapperRef;
-
-        for (let page = 1; page <= pdf.numPages; page++) {
-            const canvas = document.createElement(
-                'canvas'
-            ) as HTMLCanvasElement;
-            pdfContainer.appendChild(canvas);
-
-            pdf.getPage(page).then(function (page) {
-                const requiredScale = 2; // initial scale is set to 2 so the pdf outputs clear, keeping on 1 causes the pdf to be blurry
-
-                const viewport = page.getViewport({ scale: requiredScale });
-
-                // set canvas height/width to upscale the pdf
-                canvas.height = requiredScale * viewport.height;
-                canvas.width = requiredScale * viewport.width;
-
-                // now get the responsive viewport according to client width / actual width
-                const scale = (pdfContainer.clientWidth / viewport.width) * 1.5;
-                const reponsiveViewport = page.getViewport({ scale });
-
-                // set the canvas style as per responsive viewport if you want the pdf to take size according to screen dimensions, although this makes the pdf tidy
-                // setting it to original viewport will maintain the original size, but a scroll is needed, for now we use responsiev viewport
-                canvas.style.height = reponsiveViewport.height + 'px';
-                canvas.style.width = reponsiveViewport.width + 'px';
-
-                page.render({
-                    canvasContext: canvas.getContext('2d'),
-                    viewport: viewport,
-                    transform: [requiredScale, 0, 0, requiredScale, 0, 0],
-                });
+        try {
+            const loadingPdfTask = pdfjsLib.getDocument({
+                url: this.pdfPath,
+                withCredentials: true,
             });
-        }
+            const pdf = await loadingPdfTask.promise;
+            const pdfContainer = this.wrapperRef;
+
+            for (let page = 1; page <= pdf.numPages; page++) {
+                const canvas = document.createElement(
+                    'canvas'
+                ) as HTMLCanvasElement;
+                pdfContainer.appendChild(canvas);
+
+                pdf.getPage(page).then(function (page) {
+                    const requiredScale = 2; // initial scale is set to 2 so the pdf outputs clear, keeping on 1 causes the pdf to be blurry
+
+                    const viewport = page.getViewport({ scale: requiredScale });
+
+                    // set canvas height/width to upscale the pdf
+                    canvas.height = requiredScale * viewport.height;
+                    canvas.width = requiredScale * viewport.width;
+                    canvas.style.width = '100%';
+
+                    page.render({
+                        canvasContext: canvas.getContext('2d'),
+                        viewport: viewport,
+                        transform: [requiredScale, 0, 0, requiredScale, 0, 0],
+                    });
+                });
+            }
+        } catch (error) {}
     }
 
     render() {
