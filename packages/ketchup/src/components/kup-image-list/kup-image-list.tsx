f2849c1979220f5c9f339576bb347d80220fb89e
Leonardo-Signorelli <leonardo.signorelliv@gmail.com>
2024-04-16 12:38:29 +0200

    kup-image-list : fix mask sizes and image-list

--- packages/ketchup/src/components/kup-image-list/kup-image-list.tsx
+++ packages/ketchup/src/components/kup-image-list/kup-image-list.tsx
@@ -270,6 +270,11 @@ export class KupImageList {
                 row={{ ...node }}
             >
                 <div class="image-list__wrapper">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        version="1.1"
+                        viewBox="0 0 24 24"
+                    ></svg>
                     {image}
                     {label}
                 </div>
