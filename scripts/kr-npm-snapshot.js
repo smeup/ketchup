d5835ccb7be65586ca19c647951941f91ca86da9
Fabio Carminati <carminati1996@gmail.com>
2024-05-14 18:11:32 +0200

    test ga
    test ga

---
+++ scripts/kr-npm-snapshot.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import { spawn } from "child_process";
+import path from "path";
+import fs from "fs";
+
+function getPkg() {
+  try {
+    return JSON.parse(
+      fs.readFileSync(path.resolve(process.cwd()+"/packages/ketchup-react", "package.json"))
+    );
+  } catch (e) {
+    process.stderr.write("Could not open a package.json.", e);
+    process.exit(1);
+  }
+}
+
+function getCurrentVersion() {
+  return getPkg().version;
+}
+
+const oldVersion = getCurrentVersion() ?? error("No version in package.json found.");
+const version = oldVersion.replace(/-SNAPSHOT$/, `-SNAPSHOT.${new Date().toISOString().replace(/[-:TZ]/g, "")}`);
+console.log("Kecthup react version:",version);
+const npm = spawn(/^win/.test(process.platform) ? "npm.cmd" : "npm", [
+  "--no-git-tag-version",
+  "version",
+  version,
+]); 
+npm.stdout.pipe(process.stdout);
+npm.stderr.pipe(process.stderr);
