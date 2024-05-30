07abf73d3d22d64df42a3f6da97bc0f88cd5574d
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-05-30 23:56:46 +0200

    fix (button): improved dropdown management

--- packages/ketchup-lite/src/variables/GenericVariables.ts
+++ packages/ketchup-lite/src/variables/GenericVariables.ts
@@ -1,4 +1,6 @@
 export const CSS_VAR_PREFIX = '--kul';
+export const KUL_DROPDOWN_CLASS = 'kul-dropdown-menu';
+export const KUL_DROPDOWN_CLASS_VISIBLE = 'kul-dropdown-menu--visible';
 export const KUL_STYLE_ID = 'kul-style';
 export const KUL_WRAPPER_ID = 'kul-component';
 export const RIPPLE_SURFACE_CLASS = 'ripple-surface';
