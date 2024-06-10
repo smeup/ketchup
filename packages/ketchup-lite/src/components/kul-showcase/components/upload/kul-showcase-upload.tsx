ede732cfc8b6ff9454003d739672176256c77a72
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-06-10 18:38:33 +0200

    cicd (upload): improved tests

--- packages/ketchup-lite/src/components/kul-showcase/components/upload/kul-showcase-upload.tsx
+++ packages/ketchup-lite/src/components/kul-showcase/components/upload/kul-showcase-upload.tsx
@@ -3,6 +3,7 @@ import { UPLOAD_DOC, UPLOAD_EXAMPLES } from './kul-showcase-upload-data';
 import { SHOWCASE_DYN_EXAMPLES } from '../../kul-showcase-utils';
 import { KulShowcaseDynamicExampleType } from '../../kul-showcase-declarations';
 import { UploadExample } from './kul-showcase-upload-declarations';
+import { KulDataCyAttributes } from '../../../../types/GenericTypes';
 
 @Component({
     tag: 'kul-showcase-upload',
@@ -84,7 +85,11 @@ export class KulShowcaseUpload {
                 <div class="examples-title" part="examples-title">
                     Examples
                 </div>
-                <div class="grid" data-cy="wrapper" part="grid">
+                <div
+                    class="grid"
+                    data-cy={KulDataCyAttributes.SHOWCASE_GRID_WRAPPER}
+                    part="grid"
+                >
                     {this.#prepExamples()}
                 </div>
             </Fragment>
