d75b47e7ec1bfee078fa77b2fab7e3f0b793bafe
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-06-10 18:37:31 +0200

    cicd (switch): improved tests

--- packages/ketchup-lite/src/components/kul-showcase/components/switch/kul-showcase-switch.tsx
+++ packages/ketchup-lite/src/components/kul-showcase/components/switch/kul-showcase-switch.tsx
@@ -3,6 +3,7 @@ import { SWITCH_DOC, SWITCH_EXAMPLES } from './kul-showcase-switch-data';
 import { SHOWCASE_DYN_EXAMPLES } from '../../kul-showcase-utils';
 import { KulShowcaseDynamicExampleType } from '../../kul-showcase-declarations';
 import { SwitchExample } from './kul-showcase-switch-declarations';
+import { KulDataCyAttributes } from '../../../../types/GenericTypes';
 
 @Component({
     tag: 'kul-showcase-switch',
@@ -91,7 +92,11 @@ export class KulShowcaseSwitch {
                 <div class="examples-title" part="examples-title">
                     Examples
                 </div>
-                <div class="grid" data-cy="wrapper" part="grid">
+                <div
+                    class="grid"
+                    data-cy={KulDataCyAttributes.SHOWCASE_GRID_WRAPPER}
+                    part="grid"
+                >
                     {this.#prepExamples()}
                 </div>
             </Fragment>
