a2a84ecdb9ab8533af1a5950d440b9ee00521278
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-06-10 18:38:20 +0200

    cicd (tree): improved tests

--- packages/ketchup-lite/cypress/e2e/components/tree.cy.ts
+++ packages/ketchup-lite/cypress/e2e/components/tree.cy.ts
@@ -1,39 +1,86 @@
 import {
+    KulTreeEvent,
     KulTreeProps,
     KulTreePropsInterface,
 } from '../../../src/components/kul-tree/kul-tree-declarations';
 import { TREE_EXAMPLES_KEYS } from '../../../src/components/kul-showcase/components/tree/kul-showcase-tree-declarations';
+import { KulDataCyAttributes } from '../../../src/types/GenericTypes';
 
 const tree = 'tree';
 const treeCapitalized = tree.charAt(0).toUpperCase() + tree.slice(1);
 const treeTag = 'kul-' + tree;
 
-describe(treeTag, () => {
+describe('Basic', () => {
     beforeEach(() => {
         cy.navigate(tree);
     });
 
-    it(`common: should check that all <${treeTag}> exist`, () => {
+    it(`Should check that all <${treeTag}> exist.`, () => {
         cy.checkComponentExamples(treeTag, new Set(TREE_EXAMPLES_KEYS));
     });
 
-    it(`common: should check that the number of <${treeTag}> elements matches the number of examples`, () => {
+    it(`Should check that the number of <${treeTag}> elements matches the number of examples.`, () => {
         cy.checkComponentExamplesNumber(Array.from(TREE_EXAMPLES_KEYS));
     });
+});
 
-    it('common: should call getDebugInfo and check the structure of the returned object', () => {
+describe('Events', () => {
+    it(`click`, () => {
+        cy.navigate(tree);
+        const eventType: KulTreeEvent = 'click';
+        cy.checkEvent(tree, eventType);
+        cy.get('@eventElement')
+            .findCyElement(KulDataCyAttributes.NODE)
+            .first()
+            .click();
+        cy.getCyElement(KulDataCyAttributes.CHECK).should('exist');
+    });
+
+    it(`kul-event`, () => {
+        cy.navigate(tree);
+        const eventType: KulTreeEvent = 'kul-event';
+        cy.checkEvent(tree, eventType);
+        cy.get('@eventElement')
+            .findCyElement(KulDataCyAttributes.INPUT)
+            .first()
+            .focus();
+        cy.getCyElement(KulDataCyAttributes.CHECK).should('exist');
+    });
+
+    it(`pointerdown`, () => {
+        cy.navigate(tree);
+        const eventType: KulTreeEvent = 'pointerdown';
+        cy.checkEvent(tree, eventType);
+        cy.get('@eventElement')
+            .findCyElement(KulDataCyAttributes.NODE)
+            .first()
+            .click();
+        cy.getCyElement(KulDataCyAttributes.CHECK).should('exist');
+    });
+
+    it(`ready`, () => {
+        cy.checkReadyEvent(tree);
+    });
+});
+
+describe('Methods', () => {
+    beforeEach(() => {
+        cy.navigate(tree);
+    });
+
+    it('getDebugInfo: check the structure of the returned object.', () => {
         cy.checkDebugInfo(treeTag);
     });
 
-    it('common: should check for the presence of a <style> element with id kup-style', () => {
-        cy.checkKulStyle();
+    it('getDebugInfo, refresh: check that renderCount has increased after refreshing.', () => {
+        cy.checkRenderCountIncrease(treeTag);
     });
 
-    it(`common: should call getProps and check keys against Kul${treeCapitalized}Props enum`, () => {
+    it(`getProps: check keys against Kul${treeCapitalized}Props enum.`, () => {
         cy.checkProps(treeTag, KulTreeProps);
     });
 
-    it(`common: should call getProps and check keys against Kul${treeCapitalized}PropsInterface`, () => {
+    it(`getProps: check keys against Kul${treeCapitalized}PropsInterface.`, () => {
         cy.checkPropsInterface(treeTag, {
             kulAccordionLayout: null,
             kulData: null,
@@ -44,8 +91,14 @@ describe(treeTag, () => {
             kulStyle: null,
         } as Required<KulTreePropsInterface>);
     });
+});
 
-    it('common: should call getDebugInfo, refresh, and check that renderCount has increased', () => {
-        cy.checkRenderCountIncrease(treeTag);
+describe('Props', () => {
+    beforeEach(() => {
+        cy.navigate(tree);
+    });
+
+    it('kulStyle: should check for the presence of a <style> element with id kup-style.', () => {
+        cy.checkKulStyle();
     });
 });
