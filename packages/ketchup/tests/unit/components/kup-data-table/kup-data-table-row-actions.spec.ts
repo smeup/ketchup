49a212f49d0c423c2a15c08c5dd943537eaa525c
SerenaGiumentaroApuliasoft <serena.giumentaro@apuliasoft.com>
2024-08-12 13:58:31 +0000

    feat: unit test data-table with commands in row actions

---
+++ packages/ketchup/tests/unit/components/kup-data-table/kup-data-table-row-actions.spec.ts
@@ -0,0 +1,41 @@
+import {
+    KupCommand,
+    KupDataRowAction,
+    KupDataTableRow,
+} from '../../../../src/components';
+import { KupManager } from '../../../../src/managers/kup-manager/kup-manager';
+import { KupDom } from '../../../../src/managers/kup-manager/kup-manager-declarations';
+
+const dom: KupDom = document.documentElement as KupDom;
+if (!dom.ketchup) {
+    dom.ketchup = new KupManager();
+}
+
+import sampleKupDataDataset from '../../../resources/mock/kup-data-dataset-with-commands.json';
+
+const mockedData = sampleKupDataDataset.data;
+const mockedRows = mockedData.rows as unknown as KupDataTableRow[];
+const mockedCommands = sampleKupDataDataset.commands as unknown as KupCommand[];
+
+describe('kup datatable dataset with commands and VO;CODVER rows', () => {
+    it('should return an KupDataRowAction[] with VO;CODVER rows', () => {
+        let result: KupDataRowAction[] = [];
+
+        const expectedResult: KupDataRowAction[] = [
+            { icon: 'view-quilt', text: 'Scheda' },
+            { icon: 'delete', text: 'Elimina' },
+            { icon: 'edit', text: 'Modifica' },
+            { icon: '', text: '000052' },
+        ];
+
+        mockedRows.forEach((mockedRow) => {
+            const kupDataRowAction = dom.ketchup.data.createActionsFromVoCodRow(
+                mockedRow,
+                mockedCommands
+            );
+            result.push(...kupDataRowAction);
+        });
+
+        expect(expectedResult).toEqual(result);
+    });
+});
