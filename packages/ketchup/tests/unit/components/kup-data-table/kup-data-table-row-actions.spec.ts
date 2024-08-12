6195eae96e941afd1a96e3f29a08e55d332d4bfa
SerenaGiumentaroApuliasoft <serena.giumentaro@apuliasoft.com>
2024-08-12 15:21:52 +0000

    feat: unit test kup-data-table

--- packages/ketchup/tests/unit/components/kup-data-table/kup-data-table-row-actions.spec.ts
+++ packages/ketchup/tests/unit/components/kup-data-table/kup-data-table-row-actions.spec.ts
@@ -11,6 +11,7 @@ if (!dom.ketchup) {
     dom.ketchup = new KupManager();
 }
 
+import sampleKupDataDatasetNoCodVer from '../../../resources/mock/kup-data-dataset-with-places-and-nrs.json';
 import sampleKupDataDataset from '../../../resources/mock/kup-data-dataset-with-commands.json';
 
 const mockedData = sampleKupDataDataset.data;
@@ -18,7 +19,7 @@ const mockedRows = mockedData.rows as unknown as KupDataTableRow[];
 const mockedCommands = sampleKupDataDataset.commands as unknown as KupCommand[];
 
 describe('kup datatable dataset with commands and VO;CODVER rows', () => {
-    it('should return an KupDataRowAction[] with VO;CODVER rows', () => {
+    it('should handle both matching and non-matching VO;CODVER/commands', () => {
         let result: KupDataRowAction[] = [];
 
         const expectedResult: KupDataRowAction[] = [
@@ -36,17 +37,17 @@ describe('kup datatable dataset with commands and VO;CODVER rows', () => {
             result.push(...kupDataRowAction);
         });
 
-        expect(expectedResult).toEqual(result);
+        expect(result).toEqual(expectedResult);
     });
 
-    it('should handle VO;CODVER rows with no commands corresponding',() => {
-        const commands = []
+    it('should handle VO;CODVER rows with no commands corresponding', () => {
+        const commands = [];
         let result: KupDataRowAction[] = [];
         const expectedResult = [
             { icon: '', text: '000050' },
             { icon: '', text: '000051' },
             { icon: '', text: '000052' },
-        ]
+        ];
 
         mockedRows.forEach((mockedRow) => {
             const kupDataRowAction = dom.ketchup.data.createActionsFromVoCodRow(
@@ -56,7 +57,45 @@ describe('kup datatable dataset with commands and VO;CODVER rows', () => {
             result.push(...kupDataRowAction);
         });
 
-        expect(expectedResult).toEqual(result);
+        expect(result).toEqual(expectedResult);
+    });
 
-    } )
+    it('should handle case with no VO;CODVER rows present', () => {
+        const rows = sampleKupDataDatasetNoCodVer.rows;
+        let result: KupDataRowAction[] = [];
+
+        rows.forEach((mockedRow) => {
+            const kupDataRowAction = dom.ketchup.data.createActionsFromVoCodRow(
+                mockedRow,
+                mockedCommands
+            );
+            result.push(...kupDataRowAction);
+        });
+
+        expect(result).toEqual([]);
+    });
+});
+
+describe('kup data getCodVerRows', () => {
+    it('should filter cells with VO;CODVER from row', () => {
+        const expectedResult = [
+            {
+                isEditable: false,
+                obj: { k: '000050', p: 'COD_VER', t: 'VO' },
+                value: '000050',
+                element: { 's-hn': 'KUP-DATA-TABLE' },
+            },
+        ];
+        const result = dom.ketchup.data.getCodVerRows(mockedRows[0]);
+
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('should return an empy array where no VO;CODVER found', () => {
+        const result = dom.ketchup.data.getCodVerRows(
+            sampleKupDataDatasetNoCodVer.rows[0]
+        );
+
+        expect(result).toEqual([]);
+    });
 });
