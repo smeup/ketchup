57862b01d0e2806911b2cb1d8688606664288506
EmanueleCitarella <emanuele.citarella@apuliasoft.com>
2024-08-21 10:49:05 +0000

    fix: refuse

--- packages/ketchup/src/components/kup-data-table/kup-data-table-declarations.ts
+++ packages/ketchup/src/components/kup-data-table/kup-data-table-declarations.ts
@@ -305,7 +305,6 @@ export interface KupDatatableRowActionItemClickEventPayload
     obj?: KupObj;
     column?: KupDataColumn;
     cell?: KupDataCell;
-    cellName?: string;
     index?: number;
 }
 
