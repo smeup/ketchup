00976a071569dedd05c7fe125c2da9c228d0fcfa
Leonardo-Signorelli <leonardo.signorelliv@gmail.com>
2024-09-11 17:26:18 +0200

    Update kup-typography-declarations.ts

--- packages/ketchup/src/components/kup-typography/kup-typography-declarations.ts
+++ packages/ketchup/src/components/kup-typography/kup-typography-declarations.ts
@@ -1,7 +1,4 @@
-import { KupDataNode } from '../../managers/kup-data/kup-data-declarations';
-import { KupObj } from '../../managers/kup-objects/kup-objects-declarations';
 import { KupEventPayload } from '../../types/GenericTypes';
-import { KupListNode } from '../kup-list/kup-list-declarations';
 
 /**
  * Props of the kup-typography component.
@@ -22,29 +19,6 @@ export enum KupTypographyProps {
     BackColor = 'it is the background color of the text',
     Type = 'it is the type of the font. It is an enum that can take from different variant such as body, heading1, caption and so on',
 }
-
-/**
- * Styling options for the f-button component.
- * @enum {string}
- * @property {string} BODY - Button content align : center ( Default ).
- * @property {string} BODY_COMPACT - Button content align : center ( Default ).
- * @property {string} HEADING1 - All the content is left aligned
- * @property {string} HEADING2 - All the content is right aligned.
- * @property {string} HEADING3 - The space is perfectly divided between borders and content.
- * @property {string} CAPTION - The space between the content is the maximum.
- * @property {string} LABEL - The space is more around .
- */
-export enum KupTypographyType {
-    BODY = 'body',
-    BODY_COMPACT = 'body-compact',
-    HEADING1 = 'heading-01',
-    HEADING2 = 'heading-02',
-    HEADING3 = 'heading-03',
-    CAPTION = 'caption',
-    LABEL = 'label',
-}
-
-export interface KupTypographyIconClickEventPayload extends KupEventPayload {}
 
 export interface KupTypographyClickEventPayload extends KupEventPayload {
     value: string;
