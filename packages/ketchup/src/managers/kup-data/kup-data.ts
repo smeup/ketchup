9c69ce46db7077d848b922b6edcd9194759cfb76
SerenaGiumentaroApuliasoft <serena.giumentaro@apuliasoft.com>
2024-09-17 15:20:37 +0000

    fix: column title on codver without text

--- packages/ketchup/src/managers/kup-data/kup-data.ts
+++ packages/ketchup/src/managers/kup-data/kup-data.ts
@@ -432,12 +432,7 @@ export class KupData {
                                 codVer.value.data?.resource ||
                                 codVer.value.data?.icon ||
                                 '',
-                            text:
-                                codVer.value.icon ||
-                                codVer.value.data?.resource ||
-                                codVer.value.data?.icon
-                                    ? ''
-                                    : currentColumn.title || currentColumn.name,
+                            text: currentColumn.title || currentColumn.name, 
                             obj: codVer.value.obj,
                             cell: codVer.value,
                             type: DropDownAction.CODVER,
