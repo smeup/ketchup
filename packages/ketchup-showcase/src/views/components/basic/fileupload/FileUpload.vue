a5de777f9cb6806e2f67b88dfabda121d551e2ad
p-bemportato <pierluigi.bemportato@apuliasoft.com>
2025-01-31 10:08:34 +0000

    feat: basic showcase page

---
+++ packages/ketchup-showcase/src/views/components/basic/fileupload/FileUpload.vue
@@ -0,0 +1,27 @@
+<template>
+  <comp :giturl="giturl" :headtitle="headtitle" :titles="titles">
+    <template v-slot:0>
+      <file-upload-demo></file-upload-demo>
+    </template>
+  </comp>
+</template>
+
+<script>
+import Comp from '@/views/templates/Comp';
+import FileUploadDemo from './examples/FileUploadDemo.vue';
+export default {
+  components: {
+    Comp,
+    FileUploadDemo,
+  },
+  data() {
+    return {
+      giturl:
+        'https://github.com/smeup/ketchup/tree/develop/packages/ketchup/src/components/kup-file-upload',
+      headtitle: 'FileUpload',
+      titles: ['Playground'],
+    };
+  },
+  title: 'Ketchup | Button',
+};
+</script>
---
+++ packages/ketchup-showcase/src/views/components/basic/fileupload/examples/FileUploadDemo.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <demo
+      :demoComp="demoComp"
+      :demoEvents="demoEvents"
+      :demoMethods="demoMethods"
+      :demoProps="demoProps"
+    ></demo>
+  </div>
+</template>
+
+<script>
+import Demo from '@/views/templates/Demo';
+
+export default {
+  components: {
+    Demo,
+  },
+  name: 'FileUploadDemo',
+  data() {
+    return {
+      demoComp: createComp(),
+      demoEvents: [
+        {
+          name: 'kup-file-upload-upload',
+          type: 'upload',
+        },
+      ],
+      demoMethods: [
+        {
+          name: 'getProps',
+          description:
+            "Returns the props' values of the component. When invoked giving true as the only argument, returns the props descriptions instead.",
+        },
+        {
+          name: 'refresh',
+          description:
+            'This method is used to trigger a new render of the component.',
+        },
+        {
+          name: 'setProps',
+          description: 'Sets the props to the component.',
+        },
+      ],
+      demoProps: [
+        {
+          prop: 'customStyle',
+          description:
+            'Custom style of the component. For more information: https://smeup.github.io/ketchup/#/customization',
+          type: 'string',
+          default: '""',
+          try: 'css',
+        },
+      ],
+    };
+  },
+};
+
+function createComp() {
+  const comp = document.createElement('kup-file-upload');
+  comp.icon = 'widgets';
+  comp.id = 'demo-component';
+  comp.label = 'Demo';
+  comp.styling = 'raised';
+  const innerComp = document.createElement('kup-spinner');
+  innerComp.slot = 'spinner';
+  innerComp.dimensions = '0.6em';
+  innerComp.active = true;
+  comp.appendChild(innerComp);
+  return comp;
+}
+</script>
