b8984a2665330504ca5c48d47b846d4de640323d
Carina Gentiana Pasere <48244789+pasere-smeup@users.noreply.github.com>
2025-04-04 10:54:25 +0200

    v10.3.0

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -55,8 +55,8 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup/releases/tag/v10.2.0"
-            >v10.2.0</a
+          ><a href="https://github.com/smeup/ketchup/releases/tag/v10.3.0"
+            >v10.3.0</a
           ></div
         ></div
       >
