4c6f40b172f3df89b159e7aacdde3090b5bff829
stefanolanari <stefano.lanari@smeup.com>
2025-04-28 09:29:38 +0200

    Performance optimizations
    - It will be displayed the first 200 checkboxes when opening the filtering card.
    - Changed the logic to compute the maximum number of cells per page: if the number of columns is lower than 25, 1000 rows will be displayed; otherwise, 25,000 divided by the number of columns will be displayed.

--- packages/ketchup/src/managers/kup-language/kup-language-declarations.ts
+++ packages/ketchup/src/managers/kup-language/kup-language-declarations.ts
@@ -184,6 +184,7 @@ export enum KupLanguageGeneric {
     UPLOAD = 'genericUpload',
     CHOOSE = 'genericChoose',
     TYPE_CODE_OR_DESCR = 'genericTypeCodeOrDescr',
+    PARTIAL_FILTERS_LIST = 'genericPartialFiltersList',
 }
 /**
  * Grid decodes (data table customization settings).
