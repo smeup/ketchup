e3b22c0b919ac6bf0d930d8ababa2f66bff5ab44
Carina Gentiana Pasere <48244789+pasere-smeup@users.noreply.github.com>
2025-05-19 17:48:38 +0200

    up version 10.3.3

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -55,8 +55,8 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup/releases/tag/v10.3.2"
-            >v10.3.2</a
+          ><a href="https://github.com/smeup/ketchup/releases/tag/v10.3.3"
+            >v10.3.3</a
           ></div
         ></div
       >
