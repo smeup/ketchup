0034ab9412be01e2f85ea427ba4ff4dafff91118
santi-smeup <bruno.santi@smeup.com>
2025-07-23 10:17:49 +0200

    10.1.4

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -55,8 +55,8 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup/releases/tag/v10.1.3"
-            >v10.1.3</a
+          ><a href="https://github.com/smeup/ketchup/releases/tag/v10.1.4"
+            >v10.1.4</a
           ></div
         ></div
       >
