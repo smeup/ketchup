5e6893cb08be080d1035964594bcbe1044eee78d
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2025-12-15 09:47:14 +0100

    10.3.14

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -55,8 +55,8 @@
           @kup-tree-nodeselected="treeClick"
         ></kup-tree
         ><div class="version"
-          ><a href="https://github.com/smeup/ketchup/releases/tag/v10.3.13"
-            >v10.3.13</a
+          ><a href="https://github.com/smeup/ketchup/releases/tag/v10.3.14"
+            >v10.3.14</a
           ></div
         ></div
       >
